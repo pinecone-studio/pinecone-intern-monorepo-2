7bae7b5177ddcf1e7467481cb7a4aa504e8bad50
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2024-11-25 16:15:51 +0800

    updated post in backend IS team

--- apps/1CD/instagram/backend/src/resolvers/mutations/post/create-post.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/post/create-post.ts
@@ -1,8 +1,11 @@
 import { MutationResolvers } from '../../../generated';
 import { PostModel } from '../../../models/post.model';
+import { userModel } from '../../../models/user.model';
 
 export const createPost: MutationResolvers['createPost'] = async (_, { user, description, images }) => {
   const createdPost = await PostModel.create({ user, description, images });
-
+  if (!createdPost) {
+    throw new Error('Can not create post');
+  }
   return createdPost;
 };
