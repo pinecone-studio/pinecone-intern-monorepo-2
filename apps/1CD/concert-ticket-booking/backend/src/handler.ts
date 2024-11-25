ed27c364459e6d6639908bee035a844f2d1aabfa
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-11-25 16:12:59 +0800

    sign up with test

--- apps/1CD/concert-ticket-booking/backend/src/handler.ts
+++ apps/1CD/concert-ticket-booking/backend/src/handler.ts
@@ -5,6 +5,7 @@ import { NextRequest } from 'next/server';
 import { resolvers } from './resolvers';
 import { connectToDb } from './utils/connect-to-db';
 import { Context } from './types';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 connectToDb();
 
@@ -16,6 +17,19 @@ const server = new ApolloServer<Context>({
 
 export const handler = startServerAndCreateNextHandler<NextRequest, Context>(server, {
   context: async (req) => {
-    return { req };
+    const token = req.headers.get('authorization') || '';
+
+    let userId = null;
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+      userId = decoded.id;
+    } catch {
+      userId = null;
+    }
+
+    return {
+      userId,
+    };
   },
 });
--- apps/1CD/concert-ticket-booking/backend/src/schemas/index.ts
+++ apps/1CD/concert-ticket-booking/backend/src/schemas/index.ts
@@ -1,4 +1,5 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
+import { typeDefs as authTypesDefs } from './auth.schema';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, authTypesDefs]);
