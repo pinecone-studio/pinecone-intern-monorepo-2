4c9d06e4439135317f95a84fc08b51741296df9c
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-11-25 11:20:45 +0800

    docs(adds user schema): adds user schema

---
+++ apps/1CD/leave-request/backend/src/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import gql from "graphql-tag";
+
+export const UserTypeDefs = gql`
+  type User {
+    _id: ID!,
+    email: String!
+    userName: String!
+    profile: String,
+    role: String,
+    position: String!,
+    supervisor: [ID],
+    hireDate: Date,
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  type Query {
+    
+  }
+
+  type Mutation {
+    
+  }
+`;
