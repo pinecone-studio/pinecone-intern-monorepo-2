8cb4cbe9dc6dc2051f43dcbd780bffc6d585888c
Erdenejargal Odonkhuu <erdenejargal@Erdenejargals-MacBook-Pro.local>
2024-11-27 22:48:38 +0800

    fixing prob

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/venue/create-arena.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/venue/create-arena.ts
@@ -1,7 +1,7 @@
 import { MutationResolvers} from "../../../generated";
 import Venue from "../../../models/venue.model";
 
-export const createArena: MutationResolvers['createArena']=async(_, {input})=>{
+export const createArena: MutationResolvers['createArena']=async(_:unknown, {input})=>{
     const {name, location, image, capacity, size}=input;
     const createVenue=await Venue.create({
         name:name,
