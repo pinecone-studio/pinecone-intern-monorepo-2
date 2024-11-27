14d8a3100f7396627a3c6d68cdc697549e9102a4
Myeruyert Janibyek <24LP4972@admins-iMac.local>
2024-11-26 17:56:07 -0800

    signup with token

--- apps/1CD/instagram/backend/src/resolvers/mutations/signup.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/signup.ts
@@ -1,22 +1,31 @@
 import { MutationResolvers } from '../../generated';
 import { userModel } from '../../models/user.model';
-
-const checkExistingUser = async (email: string) => {
-  const existingUser = await userModel.findOne({ email });
-  if (existingUser) {
-    throw new Error('User already exists');
-  }
-};
+import jwt from 'jsonwebtoken';
 
 export const signup: MutationResolvers['signup'] = async (_, { input }) => {
-  try {
-    await checkExistingUser(input.email);
-    const newUser = await userModel.create(input);
-    return newUser;
-  } catch (error: unknown) {
-    if (error instanceof Error && error.message === 'User already exists') {
-      throw error;
-    }
-    throw new Error('Failed to signup');
+  const { email } = input;
+
+  const user = await userModel.findOne({ email });
+
+  if (user) throw new Error('User already exists');
+
+  const newUser = await userModel.create({
+    ...input,
+  });
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET environment variable is not set');
   }
+
+  const token = jwt.sign(
+    {
+      userId: newUser._id,
+    },
+    process.env.JWT_SECRET
+  );
+
+  return {
+    user: newUser,
+    token,
+  };
 };
