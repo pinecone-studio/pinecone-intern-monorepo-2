4f7d7c3bd267ed6861a093776dd5885a3197d0aa
muugii9902 <muugii9902@gmail.com>
2024-11-27 15:44:44 +0800

    feat(tinder-backend): forgetpass

---
+++ apps/1CD/tinder/backend/src/resolvers/mutations/user.ts/forgetpassword.ts
@@ -0,0 +1,75 @@
+import { userModel } from '../../../models';
+
+const forgetPassword = {
+  Mutation: {
+    registerEmail: async (_: unknown, { input }: { input: { email: string } }) => {
+      const { email } = input;
+
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return {
+          success: false,
+          message: 'Энэ имэйл бүртгэлгүй байна. Бүртгэл үүсгэнэ үү.',
+        };
+      }
+
+      //   const otp = generateOTP();
+
+      //   user.otp = otp;
+      await user.save();
+
+      //   await sendOTPEmail(email, otp);
+
+      return {
+        success: true,
+        message: 'OTP амжилттай илгээгдлээ.',
+        email,
+      };
+    },
+
+    registerOtp: async (_: unknown, { input }: { input: { email: string; otp: number } }) => {
+      const { email, otp } = input;
+
+      const user = await userModel.findOne({ email });
+      if (!user || user.otp !== otp) {
+        return {
+          success: false,
+          message: 'Буруу OTP эсвэл хэрэглэгч олдсонгүй.',
+        };
+      }
+
+      user.otp = undefined;
+      await user.save();
+
+      return {
+        success: true,
+        message: 'OTP амжилттай баталгаажлаа.',
+        email,
+      };
+    },
+
+    registerPassword: async (_: unknown, { input }: { input: { email: string; otp: number; password: string } }) => {
+      const { email, otp, password } = input;
+
+      const user = await userModel.findOne({ email });
+      if (!user || user.otp !== otp) {
+        return {
+          success: false,
+          message: 'Буруу OTP эсвэл хэрэглэгч олдсонгүй.',
+        };
+      }
+
+      user.password = password;
+      user.otp = undefined;
+      await user.save();
+
+      return {
+        success: true,
+        message: 'Нууц үг амжилттай тохируулагдлаа.',
+        email,
+      };
+    },
+  },
+};
+
+export default forgetPassword;
