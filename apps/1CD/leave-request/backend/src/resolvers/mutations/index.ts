62fb19c4a6520489c62430bc470dd72578aba045
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-11-28 13:05:44 +0800

    feat(creates new user): create new user mutation

--- apps/1CD/leave-request/backend/src/resolvers/mutations/index.ts
+++ apps/1CD/leave-request/backend/src/resolvers/mutations/index.ts
@@ -1,2 +1,3 @@
 export * from './sample-mutation';
 export * from './otp'
+export * from './user'
--- apps/1CD/leave-request/backend/src/schemas/user.schema.ts
+++ apps/1CD/leave-request/backend/src/schemas/user.schema.ts
@@ -1,22 +1,24 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const UserTypeDefs = gql`
   type User {
-    _id: ID!,
+    _id: ID!
     email: String!
     userName: String!
-    profile: String,
-    role: String,
-    position: String!,
-    supervisor: [ID],
-    hireDate: Date,
+    profile: String
+    role: String
+    position: String!
+    supervisor: [ID]
+    hireDate: Date
     createdAt: Date
     updatedAt: Date
   }
 
-  
+  type Mutation {
+    createUser(email: String!, userName: String!, profile: String!, role: String!, position: String!, supervisor: [ID], hireDate: Date!): User
+  }
 
   type Query {
-    findUserByEmail(email: String!):User 
+    findUserByEmail(email: String!): User
   }
 `;
