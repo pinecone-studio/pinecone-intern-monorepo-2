bc04f494139a9e4e7595b011cd1ebcf31fa4779e
Abylai12 <abilay1208@gmail.com>
2024-11-29 15:37:00 +0800

    concert: backend create event (#152)
    * feat(concert-backend): create event muatation
    
    * feat(concert-backend): create event
    
    ---------
    
    Co-authored-by: Abylai12 <abylai@Aigyerims-MacBook-Air.local>

---
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/create-event.ts
@@ -0,0 +1,22 @@
+import { MutationResolvers } from '../../../generated';
+import Event from '../../../models/event.model';
+import Ticket from '../../../models/ticket.model';
+
+export const createEvent: MutationResolvers['createEvent'] = async (_, { input }) => {
+  const newTickets = await Ticket.insertMany(input.dayTickets);
+  const ticketIds = newTickets.map((item) => item._id);
+  const scheduledDays = input.dayTickets.map((item) => item.scheduledDay);
+  const newEvent = await Event.create({
+    name: input.name,
+    description: input.description,
+    scheduledDays: scheduledDays,
+    mainArtists: input.mainArtists,
+    guestArtists: input.guestArtists,
+    dayTickets: ticketIds,
+    image: input.image,
+    venue: input.venue,
+    category: input.category,
+  });
+
+  return newEvent;
+};
