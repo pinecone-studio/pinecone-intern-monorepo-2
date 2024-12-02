5c678ab563efed8155dbb20ed456b8dd9ddab988
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-02 00:55:31 -0800

    feat(concert-backend): delete

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/delete-event.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/delete-event.ts
@@ -2,11 +2,21 @@ import { MutationResolvers } from '../../../generated';
 import Event from '../../../models/event.model';
 
 export const deleteEvent: MutationResolvers['deleteEvent'] = async (_, { _id }) => {
-  const eventDeleted = await Event.findOneAndDelete({ _id });
-  if (!eventDeleted) {
-    throw new Error('Event not found');
+  console.log({ _id });
+  try {
+    const eventDeleted = await Event.findOneAndDelete({ _id });
+    console.log({ eventDeleted });
+    if (!eventDeleted) {
+      throw new Error('Event not found');
+    }
+    console.log('delete', eventDeleted);
+    return {
+      message: 'success',
+    };
+  } catch (e) {
+    console.log({ e });
+    return {
+      message: 'error',
+    };
   }
-  return {
-    message: 'success',
-  };
 };
--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/index.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/index.ts
@@ -1,1 +1,2 @@
 export * from './create-event';
+export * from './delete-event';
