256fb0424d19f1a620b4e14933dc62a7f9a9a419
bayarmaa0715 <81559980+bayarmaa0715@users.noreply.github.com>
2024-12-02 18:28:41 +0800

    delete and get mypost in fronden IG TEAM (#156)
    * delete and get mypost in fronden IG TEAM
    
    * IG team: delete moadl and get my post modal in frontend
    
    * IG team: delete moadl and get my post modal in frontend 2
    
    * IG team: delete moadl and get my post modal in frontend 3
    
    ---------
    
    Co-authored-by: Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
    Co-authored-by: Myeruyert <myeruyertjanibyek@gmail.com>

---
+++ apps/1CD/instagram/frontend/src/components/post/DeleteModal.tsx
@@ -0,0 +1,43 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { useDeletePostMutation } from '@/generated';
+import { Dispatch, SetStateAction } from 'react';
+
+export const DeleteModal = ({ setOpenDeleteModal, openDeleteModal, id }: { setOpenDeleteModal: Dispatch<SetStateAction<boolean>>; openDeleteModal: boolean; id: string }) => {
+  const [deletePost, { loading }] = useDeletePostMutation();
+
+  const handleDalete = async () => {
+    await deletePost({
+      variables: {
+        _id: id,
+      },
+    });
+    setOpenDeleteModal(false);
+  };
+
+  //aldaaa zaawal toast uguh
+  //refresh hiih
+  return (
+    <Dialog
+      open={openDeleteModal}
+      // onOpenChange={() => setOpenDeleteModal(false)}
+    >
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Delete post?{id}</DialogTitle>
+          <DialogDescription>Are you sure you want to delete this post?</DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <Button data-testid="cancel-btn" className="text-black bg-white hover:text-white hover:bg-slate-400" onClick={() => setOpenDeleteModal(false)}>
+            Cancel
+          </Button>
+          <Button data-testid="delete-post-btn" className="text-red-500 bg-white border hover:text-black hover:bg-white hover:border-red-500" onClick={() => handleDalete()}>
+            {loading ? 'Loading ...' : 'Delete'}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
