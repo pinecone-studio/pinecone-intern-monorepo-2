54ab5198c5e2454ba2180b4c36f23e42fca10f11
24LP9968 24LP9968 <24LP9968@administrators-iMac.local>
2024-12-02 18:10:11 +0800

    feat: tinder birthday input

--- apps/1CD/tinder/frontend/src/app/date-of-birth/input.tsx
+++ apps/1CD/tinder/frontend/src/app/date-of-birth/input.tsx
@@ -35,33 +35,31 @@ export function CalendarForm({ setDob }: CalendarFormProps) {
   };
 
   return (
-    <Form {...form}>
-      <form className="space-y-8">
-        <FormField
-          control={form.control}
-          name="dob"
-          render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <FormLabel htmlFor="dob">Date of birth (Required)</FormLabel>
-              <Popover>
-                <PopoverTrigger asChild>
-                  <FormControl>
-                    <Button variant="outline" className={cn('w-[294px] pl-3 text-left font-normal', !field.value && 'text-muted-foreground')} aria-label="Pick a date of birth">
-                      {field.value ? format(field.value, 'PPP') : <span>Pick a date</span>}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
-                  </FormControl>
-                </PopoverTrigger>
-                <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar mode="single" selected={field.value} onSelect={handleDateSelect} disabled={(date) => date > new Date() || date < new Date('1900-01-01')} initialFocus />
-                </PopoverContent>
-              </Popover>
-              <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
+    <form className="space-y-8">
+      <FormField
+        control={form.control}
+        name="dob"
+        render={({ field }) => (
+          <FormItem className="flex flex-col">
+            <FormLabel htmlFor="dob">Date of birth (Required)</FormLabel>
+            <Popover>
+              <PopoverTrigger asChild>
+                <FormControl>
+                  <Button variant="outline" className={cn('w-[294px] pl-3 text-left font-normal', !field.value && 'text-muted-foreground')} aria-label="Pick a date of birth">
+                    {field.value ? format(field.value, 'PPP') : <span>Pick a date</span>}
+                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                  </Button>
+                </FormControl>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar mode="single" selected={field.value} onSelect={handleDateSelect} disabled={(date) => date > new Date() || date < new Date('1900-01-01')} initialFocus />
+              </PopoverContent>
+            </Popover>
+            <FormDescription>Your date of birth is used to calculate your age.</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </form>
   );
 }
