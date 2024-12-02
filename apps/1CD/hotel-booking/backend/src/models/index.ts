ec7ec382502e9567526c66bdb1a01b1f23ba8488
badral <badralbaigalmaa7@gmail.com>
2024-12-02 14:48:20 +0800

    feat(hotel booking backend): payment create method done (#173)
    Co-authored-by: badral <badral@badrals-MacBook-Pro.local>

--- apps/1CD/hotel-booking/backend/src/models/index.ts
+++ apps/1CD/hotel-booking/backend/src/models/index.ts
@@ -2,5 +2,5 @@ export * from './amenities.model';
 export * from './hotels.model';
 export * from './rooms.model';
 export * from './user.model';
-export * from './payment.mode';
+export * from './payment.model';
 export * from './booking.model';
