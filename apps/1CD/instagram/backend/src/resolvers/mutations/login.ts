99c7e48d75d99b34945332f092c4afc03cb7d528
amiekhb <aminaaami0715@gmail.com>
2024-12-03 10:36:40 +0800

    instagram-backend-login

---
+++ apps/1CD/instagram/backend/src/resolvers/mutations/login.ts
@@ -0,0 +1,19 @@
+import { MutationResolvers } from '../../generated';
+import { userModel } from '../../models/user.model';
+import jwt from 'jsonwebtoken';
+
+export const login: MutationResolvers['login'] = async (_, { input }) => {
+  const { email, password } = input;
+  const user = await userModel.findOne({
+    email,
+    password,
+  });
+  if (!user) throw new Error('Invalid credentials');
+  const token = jwt.sign(
+    {
+      userId: user._id,
+    },
+    process.env.JWT_SECRET!
+  );
+  return { user, token };
+};
