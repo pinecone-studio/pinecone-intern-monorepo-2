739bf9c90d41d983e7e4b4b87d8ccb93e347bd32
satsuralala <satsuraltumurbat@gmail.com>
2024-12-03 12:56:07 +0800

    feat: (tinder):added email frotnend (#236)
    * feat: (tinder):added email frotnend
    
    * feat: (tinder):added frontend of email and fixed preview
    
    * feat: (tinder): fixed preview again

--- apps/1CD/tinder/frontend/src/components/providers/ApolloWrapper.tsx
+++ apps/1CD/tinder/frontend/src/components/providers/ApolloWrapper.tsx
@@ -5,7 +5,7 @@ import { ApolloNextAppProvider, ApolloClient, InMemoryCache } from '@apollo/expe
 import { PropsWithChildren } from 'react';
 import { setContext } from '@apollo/client/link/context';
 
-const uri = process.env.BACKEND_URI;
+const uri =process.env.LOCAL_BACKEND_URI ?? process.env.BACKEND_URI;
 
 const makeClient = () => {
   const httpLink = new HttpLink({
