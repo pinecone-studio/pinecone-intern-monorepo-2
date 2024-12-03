0707eee335c0710d931f6c70389548a676c743c1
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2024-12-03 17:18:02 +0800

    home do

--- apps/1CD/instagram/frontend/src/app/page.tsx
+++ apps/1CD/instagram/frontend/src/app/page.tsx
@@ -5,7 +5,8 @@ import { PostCard } from '@/components/post/PostCard';
 const Page = () => {
   return (
     <div>
-      Home Page
+      <p> Home Page .. </p>
+
       <PostCard />
     </div>
   );
