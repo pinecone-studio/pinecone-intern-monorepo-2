bca4fcb8328f048cfe15c746ae94d58484d098c9
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-03 00:12:57 -0800

    feat(concert-backend): filter test added

--- apps/1CD/concert-ticket-booking/backend/specs/resolvers/queries/event/get-events.spec.ts
+++ apps/1CD/concert-ticket-booking/backend/specs/resolvers/queries/event/get-events.spec.ts
@@ -30,6 +30,34 @@ describe('Get Events', () => {
   });
 
   it('should return events', async () => {
+    const result = await getEvents!({}, { filter: { date: '2024-11-28T18:00' } }, { userId: null }, {} as GraphQLResolveInfo);
+    expect(result).toEqual([
+      {
+        _id: '1',
+        name: 'test-name',
+      },
+      {
+        _id: '2',
+        name: '2',
+      },
+    ]);
+  });
+
+  it('should return events', async () => {
+    const result = await getEvents!({}, { filter: { artist: 'rock' } }, { userId: null }, {} as GraphQLResolveInfo);
+    expect(result).toEqual([
+      {
+        _id: '1',
+        name: 'test-name',
+      },
+      {
+        _id: '2',
+        name: '2',
+      },
+    ]);
+  });
+
+  it('should return events', async () => {
     const result = await getEvents!({}, {}, { userId: null }, {} as GraphQLResolveInfo);
     expect(result).toEqual([
       {
