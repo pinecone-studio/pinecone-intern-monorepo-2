90e4a6d4cf48c0e6551cdb303d24e37367aa0215
Galt <164704929+Galtbatzana@users.noreply.github.com>
2024-12-03 18:53:58 +0800

    feat(hotel-booking): hotel-booking backend (#269)
    hotel-booking backend get-hotels

---
+++ apps/1CD/hotel-booking/backend/specs/resolvers/queries/hotel/get-hotels.spec.ts
@@ -0,0 +1,36 @@
+import { GraphQLResolveInfo } from 'graphql';
+
+import { getHotels } from 'src/resolvers/queries';
+
+jest.mock('src/models', () => ({
+  hotelsModel: {
+    find: jest
+      .fn()
+      .mockReturnValueOnce([
+        {
+          _id: '1',
+          hotelName: 'test',
+        },
+      ])
+      .mockReturnValueOnce([]),
+  },
+}));
+
+describe('get-hotels', () => {
+  it('should return hotels', async () => {
+    const response = await getHotels!({}, {}, { userId: '1' }, {} as GraphQLResolveInfo);
+    expect(response).toEqual([
+      {
+        _id: '1',
+        hotelName: 'test',
+      },
+    ]);
+  });
+  it('should return hotels', async () => {
+    try {
+      await getHotels!({}, {}, { userId: '1' }, {} as GraphQLResolveInfo);
+    } catch (err) {
+      expect((err as Error).message).toEqual('Hotels not found');
+    }
+  });
+});
