537876526c13c5c864a6b799b34685db2ebeb5cf
amiekhb <aminaaami0715@gmail.com>
2024-12-03 10:36:40 +0800

    instagram-backend-login

--- apps/1CD/instagram/backend/src/resolvers/mutations/index.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/index.ts
@@ -4,3 +4,4 @@ export * from './post';
 export * from './follow/send-follow-req';
 export * from './user/update-user-mutation';
 export * from './user/delete-user-mutation';
+export * from './login';
