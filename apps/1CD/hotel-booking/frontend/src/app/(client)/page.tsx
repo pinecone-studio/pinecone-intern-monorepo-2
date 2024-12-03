08fd65179fe9d1f95f4f3c0e7513cc2329e62809
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-12-03 19:58:55 +0800

    home page

---
+++ apps/1CD/hotel-booking/frontend/src/app/(client)/page.tsx
@@ -0,0 +1,31 @@
+const Page = () => {
+  return (
+    <div className="max-w-[1920px]">
+      <div className="container mx-auto">
+        <div className="flex justify-between pt-8 pb-4">
+          <p className="font-semibold text-[24px] texxt-[#09090B]">Popular Hotels</p>
+          <button className="border-2 font-medium text-[14px] text-[#18181B] py-2 px-4 rounded-md">View all</button>
+        </div>
+
+        <div className="grid grid-cols-4 rounded-md">
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">1</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">2</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">3</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">4</div>
+        </div>
+
+        <div className="flex justify-between pt-8 pb-4">
+          <p className="font-semibold text-[24px] texxt-[#09090B]">Most booked hotels in Mongolia in past month</p>
+          <button className="border-2 font-medium text-[14px] text-[#18181B] py-2 px-4 rounded-md">View all</button>
+        </div>
+        <div className="grid grid-cols-4 rounded-md pb-14">
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">1</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">2</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">3</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">4</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Page;
