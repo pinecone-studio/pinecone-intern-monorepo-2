941016f11a7ea17dd3ae30577d2d1464865d25b0
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-02 19:21:00 -0800

    feat(concert-backend): last

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/delete-event.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/delete-event.ts
@@ -2,14 +2,12 @@ import { MutationResolvers } from '../../../generated';
 import Event from '../../../models/event.model';
 
 export const deleteEvent: MutationResolvers['deleteEvent'] = async (_, { _id }) => {
-  console.log({ _id });
   try {
     const eventDeleted = await Event.findOneAndDelete({ _id });
     console.log({ eventDeleted });
     if (!eventDeleted) {
       throw new Error('Event not found');
     }
-    console.log('delete', eventDeleted);
     return {
       message: 'success',
     };
