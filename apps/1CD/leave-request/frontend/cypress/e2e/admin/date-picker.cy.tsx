c017a9aa12c64df56f44e98d8ccdaad628e45420
Galbadrakh <galbadrakh223@gmail.com>
2024-12-04 12:31:00 +0800

    Admin table section Front-End setup (#277)
    Admin section front-end setup
    
    Co-authored-by: galbadrakh <galbadrak223@gmail.com>
    Co-authored-by: Zoljargal2004 <zolookorzoloo@gmail.com>

---
+++ apps/1CD/leave-request/frontend/cypress/e2e/admin/date-picker.cy.tsx
@@ -0,0 +1,11 @@
+describe('DatePicker Component', () => {
+  beforeEach(() => {
+    cy.visit('/admin');
+  });
+  it('should navigate to previous month when clicking previous button', () => {
+    cy.get('[aria-label="Previous month"]').should('exist');
+    cy.get('[aria-label="Previous month"]').click();
+    cy.get('[aria-label="Next month"]').should('exist');
+    cy.get('[aria-label="Next month"]').click();
+  });
+});
---
+++ apps/1CD/leave-request/frontend/src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/admin/Header';
+import Table from '@/components/admin/Table';
+
+import React from 'react';
+
+const AdminPage = () => {
+  return (
+    <>
+      <Header />
+      <Table />
+    </>
+  );
+};
+
+export default AdminPage;
