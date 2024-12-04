b72ec01da17c4d7c66e113c49cae7b4aa3dd3036
Odonkhuu Munkhzul <bolzop.zulaa@gmail.com>
2024-12-04 14:26:23 +0800

    feat(frontend): user my request page

---
+++ apps/1CD/leave-request/frontend/src/app/(user)/MyRequest/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Requests from '@/components/myreq/requests';
+import UserHeader from '@/components/myreq/UserHeader';
+
+const Page = () => {
+  return (
+    <>
+      <UserHeader />
+      <Requests />
+    </>
+  );
+};
+
+export default Page;
---
+++ apps/1CD/leave-request/frontend/src/components/myreq/Requests.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent } from '@/components/ui/card';
+
+const Requests = () => {
+  return (
+    <>
+      <div className='w-full h-full bg-[#F4F4F5] '>
+        <div className='w-[214px] h-[60px] bg-slate-300'>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Requests;
+
