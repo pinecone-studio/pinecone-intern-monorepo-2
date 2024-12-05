81ba7eb3559eb0160af9e3bc87760e153ef72f6d
Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>
2024-12-05 19:38:17 +0800

    feat: dd

--- apps/1CD/hotel-booking/frontend/src/app/page.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { SearchResult } from '@/components/search-hotel/SearchResult';
+
 const Page = () => {
   return (
     <>
       <section className="h-screen">
         <h2>Home Page</h2>
       </section>
+      <SearchResult />
     </>
   );
 };
