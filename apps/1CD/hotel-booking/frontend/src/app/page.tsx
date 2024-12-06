61cc9dbcbed9464056889c8038f4aeb7e6d22c7e
Dwjrgal <dwjrgl651@gmail.com>
2024-12-06 19:02:54 +0800

    246 frontend search result (#316)
    * feat: frontend:search result
    
    * feat: home page
    
    * feat: some change in e2e test
    
    * feat: kk
    
    * feat: dd
    
    * feat: ss
    
    * feat(hotel-booking-frontend): ss
    
    ---------
    
    Co-authored-by: Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>

--- apps/1CD/hotel-booking/frontend/src/app/page.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/page.tsx
@@ -1,7 +1,16 @@
 'use client';
 
+import { SearchResult } from '@/components/search-hotel/SearchResult';
+
 const Page = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section className="h-screen">
+        <h2>Home Page</h2>
+        <SearchResult />
+      </section>
+    </>
+  );
 };
 
 export default Page;
