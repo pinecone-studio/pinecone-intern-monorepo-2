dd2142d692115897fb2d418b71b385829e4a128a
satsuralala <satsuraltumurbat@gmail.com>
2024-12-09 17:19:03 +0800

    feat: tinder:fixed backend for cypress

--- apps/1CD/tinder/backend/specs/utils/user/generateOtp.spec.ts
+++ apps/1CD/tinder/backend/specs/utils/user/generateOtp.spec.ts
@@ -3,7 +3,8 @@ import { generateOTP } from "../../../src/utils/user/generate-otp";
 
 describe('generate otp', ()=>{
     it('it should generate the otp',()=>{
-        const res=generateOTP();
+        const mockEmail='cypress@gmail.com'
+        const res=generateOTP(mockEmail);
         expect(res).toBeGreaterThan(1000)
         expect(res).toBeLessThan(9999);
         expect(typeof res).toBe('number');
