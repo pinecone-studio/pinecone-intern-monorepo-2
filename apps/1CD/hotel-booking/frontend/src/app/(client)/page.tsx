afec64ecabcb4a7f57b2fbb144c187ad4d9a1670
Galt <164704929+Galtbatzana@users.noreply.github.com>
2024-12-09 17:05:29 +0800

    270 hotel booking frontend homepage (#359)
    * home page
    
    * feat(hotel-booking): frontend map
    
    * feat(hotel-booking-frontend): add image
    
    * feat(hotel-booking-frontend): get hotels
    
    * feat(hotel-booking-frontend): homepage image
    
    homepage cypress

---
+++ apps/1CD/hotel-booking/frontend/src/app/(client)/page.tsx
@@ -0,0 +1,50 @@
+'use client';
+import HomePageCard from '@/components/HomePageCard';
+import { SearchResult } from '@/components/search-hotel/SearchResult';
+import { useGetHotelsQuery } from '@/generated';
+
+const Page = () => {
+  const { data, loading } = useGetHotelsQuery();
+  // data = {
+  //   getHotels : [
+  //     {
+
+  //     }
+  //   ]
+  // }
+  console.log({ data });
+
+  if (loading) return <div>loading...</div>;
+  return (
+    <div data-cy="Home-Page" className="max-w-[1920px]">
+      <SearchResult />
+
+      <div className="container mx-auto">
+        <div className="flex justify-between pt-8 pb-4">
+          <p className="font-semibold text-[24px] texxt-[#09090B]">Popular Hotels</p>
+          <button className="border-2 font-medium text-[14px] text-[#18181B] py-2 px-4 rounded-md">View all</button>
+        </div>
+
+        <div className="grid grid-cols-4 rounded-md gap-2">
+          {data?.getHotels.map((hotel) => (
+            <div key={hotel._id}>
+              <HomePageCard hotel={hotel} />
+            </div>
+          ))}
+        </div>
+
+        <div className="flex justify-between pt-8 pb-4">
+          <p className="font-semibold text-[24px] texxt-[#09090B]">Most booked hotels in Mongolia in past month</p>
+          <button className="border-2 font-medium text-[14px] text-[#18181B] py-2 px-4 rounded-md">View all</button>
+        </div>
+        <div className="grid grid-cols-4 rounded-md pb-14">
+          <div>1</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">2</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">3</div>
+          <div className="w-[308px] h-[424px] bg-pink-500 rounded-md">4</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Page;
