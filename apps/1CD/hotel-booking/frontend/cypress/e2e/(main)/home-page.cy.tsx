4f99b0e07de55e71ad43ae93deeffe2b5c6edcd2
badral <badral@badrals-MacBook-Pro.local>
2024-12-09 15:50:03 +0800

    feat(hotel-booking-frontend): payment page

--- apps/1CD/hotel-booking/frontend/cypress/e2e/(main)/home-page.cy.tsx
+++
@@ -1,9 +0,0 @@
-describe('Home Page', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
-  it('1. Should render add-hotel', () => {
-    cy.get('[data-cy=hello]').should('be.visible');
-  });
-});
