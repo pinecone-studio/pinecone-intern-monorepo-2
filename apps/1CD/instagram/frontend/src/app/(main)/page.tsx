335ffdd9e15af065ed8b418ff730902ffe64fa2b
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2024-12-10 10:30:11 +0800

    navbar done

--- apps/1CD/instagram/frontend/src/app/(main)/page.tsx
+++ apps/1CD/instagram/frontend/src/app/(main)/page.tsx
@@ -1,11 +1,11 @@
 'use client';
-
+import React from 'react';
 import { PostCard } from '@/components/post/PostCard';
 
 const Page = () => {
   return (
     <div>
-      Home Page
+      <p> Home Page .. </p>
       <PostCard />
     </div>
   );
