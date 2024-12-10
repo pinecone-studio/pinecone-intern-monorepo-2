15991551a102327d2e30baf34cdbe135d3458ff7
Your Nameclear <amarjargal.ts01@gmail.com>
2024-12-10 17:18:51 +0800

    supervisor front layout

---
+++ apps/1CD/leave-request/frontend/src/app/supervisor/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/supervisor/Header';
+
+import React from 'react';
+
+const SupervisorPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default SupervisorPage;
