f783af8e1ec816fca2d3714362545de888b2c1ab
24LP4508 24LP4508 <24LP4508@administrators-iMac.local>
2024-12-10 15:34:32 +0800

    fix: get rooms test aldaa zasaw

--- apps/1CD/hotel-booking/backend/src/resolvers/queries/rooms/get-room-data.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/rooms/get-room-data.ts
@@ -1,13 +1,12 @@
 import { roomsModel } from 'src/models';
 import { QueryResolvers } from '../../../generated';
  
- 
 export const getRooms: QueryResolvers['getRooms'] = async (_,variables) => {
   const {hotelId} = variables;
  
   const rooms = await roomsModel.find({hotelId});
  
-  if(!rooms.length){
+  if(!rooms){
     throw new Error('Rooms not found');
   }
   return rooms;
