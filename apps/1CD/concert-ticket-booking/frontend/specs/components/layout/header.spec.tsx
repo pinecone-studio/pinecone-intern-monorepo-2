e3cfd8d9406058ee6010b2f81d924512e5ee0131
Tuul29 <164708089+Tuul29@users.noreply.github.com>
2024-12-10 20:05:21 +0800

    feat(header&footer): concert-frontend (#328)
    * feat(header&footer): concert-frontend
    
    * feat(concert frontend): header footer
    
    * feat(header and footer): header and Footer, sign-in sign-up desing
    
    * feat(header&footer): header&footer, frontend desing
    
    * feat(concert-front): header& footer edit
    
    * feat(concert-front): forntend-edit
    
    * fix(concert-frontend): sign up
    
    * feat(concert-frontend): header and footer done
    
    * fix(concert-frontend): nx json
    
    * feat(concert-frontend): jest test in signIn, header, signUp
    
    * feat(concert-frontend): remove header cypress test
    
    * fix(concert-frontend): create spec folders
    
    * fix(concert-frontend): fix
    
    ---------
    
    Co-authored-by: Цэгмэд Туул <24LP5199@administrators-iMac-3.local>

---
+++ apps/1CD/concert-ticket-booking/frontend/specs/components/layout/header.spec.tsx
@@ -0,0 +1,33 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { Header } from '../../../src/components/Header';
+import { useAuth } from '@/components/providers/AuthProvider';
+import { jest } from '@jest/globals';
+
+jest.mock('@/components/providers/AuthProvider');
+
+const mockSignout = jest.fn();
+
+(useAuth as jest.Mock).mockReturnValue({
+  user: null,
+  signout: mockSignout,
+});
+
+describe('Header Component', () => {
+  it('renders the header elements', () => {
+    const { getByTestId } = render(<Header />);
+    expect(getByTestId('SignUpBtn'));
+    expect(getByTestId('SignInBtn'));
+  });
+
+  it('displays the SignOut button when user is present', () => {
+    (useAuth as jest.Mock).mockReturnValue({
+      user: { name: 'Test User' },
+      signout: mockSignout,
+    });
+
+    const { getByTestId } = render(<Header />);
+
+    expect(getByTestId('SignOutBtn'));
+  });
+});
--- apps/1CD/concert-ticket-booking/frontend/src/app/(user)/sign-in/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/(user)/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import SignIn from '@/components/auth/SignIn';
+import SignIn from '@/components/SignIn';
 import React from 'react';
 
 const LoginPage = () => {
