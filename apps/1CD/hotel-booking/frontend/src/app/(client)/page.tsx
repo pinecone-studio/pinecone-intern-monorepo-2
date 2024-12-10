7f53764248e583ca4116a06492139e4e52284f30
Dwjrgal <dwjrgl651@gmail.com>
2024-12-10 19:46:10 +0800

    350 search hotels (#426)
    * feat: search res
    
    * feat: filters component
    
    ---------
    
    Co-authored-by: Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>

--- apps/1CD/hotel-booking/frontend/src/app/(client)/page.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(client)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import HomePageCard from '@/components/HomePageCard';
-import { SearchResult } from '@/components/search-hotel/SearchResult';
+
 import { useGetHotelsQuery } from '@/generated';
 import Link from 'next/link';
 
@@ -9,7 +9,6 @@ const Page = () => {
   if (loading) return <div>loading...</div>;
   return (
     <div data-cy="Home-Page" className="max-w-[1920px]">
-      <SearchResult />
       <div className="container mx-auto">
         <div className="flex justify-between pt-8 pb-4">
           <p data-cy="Popular-Hotels" className="font-semibold text-[24px] texxt-[#09090B]">
@@ -17,7 +16,7 @@ const Page = () => {
           </p>
           <button className="border-2 font-medium text-[14px] text-[#18181B] py-2 px-4 rounded-md">View all</button>
         </div>
-        <div className="grid grid-cols-4 rounded-md gap-2">
+   <div className="grid grid-cols-4 rounded-md gap-2">
           {data?.getHotels.map((hotel) => (
             <Link href="/HomeDetails" key={hotel._id}>
               <HomePageCard hotel={hotel} />
