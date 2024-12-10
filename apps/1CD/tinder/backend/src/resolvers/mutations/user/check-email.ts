97b36aa3f4148f306268c6679c48797e48019971
satsuralala <satsuraltumurbat@gmail.com>
2024-12-10 15:26:30 +0800

    feat: added string

--- apps/1CD/tinder/backend/src/resolvers/mutations/user/check-email.ts
+++ apps/1CD/tinder/backend/src/resolvers/mutations/user/check-email.ts
@@ -6,7 +6,6 @@ import { sendOtpMail } from '../../../utils/user/send-otp-email';
 export const checkEmail: MutationResolvers['checkEmail'] = async (_, { input }) => {
   const { email } = input;
   const user = await userModel.findOne({ email });
-
   if (!user) {
     throw new GraphQLError('Email not found', {
       extensions: { code: 'EMAIL_NOT_FOUND' },
