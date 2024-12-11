083673be21fed1e46e7de3b289ec017866a3137e
bayarmaa0715 <81559980+bayarmaa0715@users.noreply.github.com>
2024-12-11 15:15:48 +0800

    connect in userId in post (#439)
    Co-authored-by: Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>

--- apps/1CD/instagram/frontend/specs/components/post/PostCard.spec.tsx
+++ apps/1CD/instagram/frontend/specs/components/post/PostCard.spec.tsx
@@ -1,38 +1,113 @@
 import { fireEvent, render, waitFor } from '@testing-library/react';
-import { MockedProvider, MockedResponse } from '@apollo/client/testing';
-import { GetMyPostsDocument } from '@/generated';
+import { MockedProvider } from '@apollo/client/testing';
+import { GetMyPostsDocument, GetUserDocument } from '@/generated';
 import { PostCard } from '@/components/post/PostCard';
 
-const myPostMock: MockedResponse = {
-  request: {
-    query: GetMyPostsDocument,
-    variables: {
-      userID: '673f6ec003387ea426252c1a',
+const myPostMock = [
+  {
+    request: {
+      query: GetUserDocument,
     },
-  },
-  result: {
-    data: {
-      getMyPosts: [
-        {
-          _id: '1',
-          user: '673f6ec003387ea426252c1a',
-          description: "Test's des",
-          images: ['http://img'],
-          lastComments: 'String',
-          commentCount: 1,
-          likeCount: 2,
-          updatedAt: '2024',
-          createdAt: '2024',
+    result: {
+      data: {
+        getUser: {
+          _id: '123',
+          userName: 'test',
+          profileImg: 'http://img1',
         },
-      ],
+      },
     },
   },
-};
+
+  {
+    request: {
+      query: GetMyPostsDocument,
+      variables: {
+        userId: '123',
+      },
+    },
+
+    result: {
+      data: {
+        getMyPosts: [
+          {
+            _id: '1',
+            user: '123',
+            description: "Test's des",
+            images: ['http://img'],
+            lastComments: 'String',
+            commentCount: 1,
+            likeCount: 2,
+            updatedAt: '2024',
+            createdAt: '2024',
+          },
+        ],
+      },
+    },
+  },
+];
+
+const myPostMockNoImg = [
+  {
+    request: {
+      query: GetUserDocument,
+    },
+    result: {
+      data: {
+        getUser: {
+          _id: '123',
+          userName: 'test',
+        },
+      },
+    },
+  },
+
+  {
+    request: {
+      query: GetMyPostsDocument,
+      variables: {
+        userId: '123',
+      },
+    },
+
+    result: {
+      data: {
+        getMyPosts: [
+          {
+            _id: '1',
+            user: '123',
+            description: "Test's des",
+            images: ['http://img'],
+            lastComments: 'String',
+            commentCount: 1,
+            likeCount: 2,
+            updatedAt: '2024',
+            createdAt: '2024',
+          },
+        ],
+      },
+    },
+  },
+];
 
 describe('getMyPost', () => {
   it('should render', async () => {
     const { getByTestId } = render(
-      <MockedProvider mocks={[myPostMock]}>
+      <MockedProvider mocks={myPostMock}>
+        <PostCard />
+      </MockedProvider>
+    );
+
+    await waitFor(() => expect(getByTestId('post-card')));
+    const moreBtn = getByTestId('more-btn');
+    fireEvent.keyDown(moreBtn, { key: 'Enter' });
+    const deleteBtn = getByTestId('delete-btn');
+    fireEvent.click(deleteBtn);
+  });
+
+  it('should render', async () => {
+    const { getByTestId } = render(
+      <MockedProvider mocks={myPostMockNoImg}>
         <PostCard />
       </MockedProvider>
     );
