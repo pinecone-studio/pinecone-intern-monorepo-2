00407e0533822923df0886e83fabe9ae0f237882
Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>
2024-12-13 14:41:42 +0800

    feat: fff

--- apps/1CD/hotel-booking/frontend/specs/components/Footer-Home.spec.tsx
+++ apps/1CD/hotel-booking/frontend/specs/components/Footer-Home.spec.tsx
@@ -2,7 +2,7 @@ import FooterHome from '@/components/FooterHome';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-describe('Footer', () => {
+describe('Footer Home', () => {
   it('should render successfully', async () => {
     render(<FooterHome />);
   });
