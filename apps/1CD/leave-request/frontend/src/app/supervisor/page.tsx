2632f12d89be2d0cee60fc3a55f55a23be1d2732
Ц.Амаржаргал <164707996+AmaraaTs@users.noreply.github.com>
2024-12-13 14:35:46 +0800

    416 leave req supervisor front v1 (#501)
    * supervisor front layout
    
    * supervisor requests header
    
    * supervisor front request list
    
    * supervisor front request detail
    
    * supervisor front request detail
    
    * supervisor front detail

---
+++ apps/1CD/leave-request/frontend/src/app/supervisor/page.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/supervisor/Header';
+import Requests from '@/components/supervisor/Requests';
+
+import React from 'react';
+
+const SupervisorPage = () => {
+  return (
+    <>
+      <Header />
+      <Requests/>
+    </>
+  );
+};
+
+export default SupervisorPage;
