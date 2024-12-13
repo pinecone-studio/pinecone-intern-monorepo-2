6abb1bff9d705a07ee02d42eb889c244af1311f2
Myeruyert <myeruyertjanibyek@gmail.com>
2024-12-12 23:37:40 -0800

    frontend-notification-component (#509)
    * frontend-notification-component
    
    * insta-front-notification-component
    
    ---------
    
    Co-authored-by: Myeruyert Janibyek <24LP4972@admins-iMac.local>

--- apps/1CD/instagram/frontend/src/app/(main)/page.tsx
+++ apps/1CD/instagram/frontend/src/app/(main)/page.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { UserBar } from '@/components/header/UserBar';
+import Notification from '@/components/notification';
 import { PostCard } from '@/components/post/PostCard';
 
 const Page = () => {
   return (
-    <div className="flex justify-between w-3/5 p-10 m-auto">
-      <div className="flex flex-col items-center ">
-        <PostCard />
+    <>
+      <Notification />
+      <div className="flex justify-between w-3/5 p-10 m-auto">
+        <div className="flex flex-col items-center ">
+          <PostCard />
+        </div>
+        <UserBar />
       </div>
-      <UserBar />
-    </div>
+    </>
   );
 };
 
