f8de66572f81b83b9275e38ad115fb287518a025
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-12-15 00:44:47 +0800

    feat(concert-frontend): create event

---
+++ apps/1CD/concert-ticket-booking/frontend/src/app/admin/home/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreateEventModal from './EventModal';
+
+const homePage = () => {
+  return (
+    <div className="h-full">
+      <h1>Admin home Page</h1>
+      <div className="h-full">
+        <CreateEventModal />
+      </div>
+    </div>
+  );
+};
+
+export default homePage;
--- apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
@@ -1,20 +1,7 @@
-'use client';
-import CardTicket from '@/components/Card';
-import { Event, useGetEventsQuery } from '@/generated';
+import React from 'react';
 
-const Page = () => {
-  const { data, loading } = useGetEventsQuery();
-  console.log(data?.getEvents);
-
-  return (
-    <div className="w-full h-screen  bg-black pt-10" data-cy="Home-Page">
-      <div className="xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
-        {loading && <div className="flex w-full h-full justify-center items-center">Loading...</div>}
-        {data?.getEvents?.map((event) => (
-          <div key={event?._id}>{event && <CardTicket event={event as Event} />}</div>
-        ))}
-      </div>
-    </div>
-  );
+const HomePage = () => {
+  return <div>User home page</div>;
 };
-export default Page;
+
+export default HomePage;
