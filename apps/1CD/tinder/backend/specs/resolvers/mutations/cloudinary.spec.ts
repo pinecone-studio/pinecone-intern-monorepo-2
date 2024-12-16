097a01ba6ec2c2bc4b2a2bb1618b71180ce014f0
24LP9968 24LP9968 <24LP9968@administrators-iMac.local>
2024-12-16 18:49:23 +0800

    feat: tinder cloudinary

---
+++ apps/1CD/tinder/backend/specs/resolvers/mutations/cloudinary.spec.ts
@@ -0,0 +1,41 @@
+import { uploadFilesInCloudinary } from '../../../src/utils/user/cloudinary';
+
+global.fetch = jest.fn();
+
+describe('uploadFilesInCloudinary', () => {
+  it('should upload file and return the secure URL', async () => {
+    const mockFile = new File(['mockBlob'], 'mock-image.jpg', { type: 'image/jpg' });
+
+    const mockResponse = {
+      secureUrl: 'https://res.cloudinary.com/test/image/upload/vXy4Tg2D/sample.jpg',
+    };
+
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      ok: true,
+      json: jest.fn().mockResolvedValueOnce(mockResponse),
+    });
+
+    const result = await uploadFilesInCloudinary(mockFile);
+
+    expect(fetch).toHaveBeenCalledWith(
+      expect.stringContaining('https://api.cloudinary.com/v1_1/dkylvahwz/upload'),
+      expect.objectContaining({
+        method: 'POST',
+        body: expect.any(FormData),
+      })
+    );
+
+    expect(result).toBe(mockResponse.secureUrl);
+  });
+
+  it('should handle error and return an empty string if upload fails', async () => {
+    const mockFile = new File(['mockBlob'], 'mock-image.jpg', { type: 'image/jpg' });
+
+    (fetch as jest.Mock).mockRejectedValueOnce(new Error('Upload failed'));
+
+    const result = await uploadFilesInCloudinary(mockFile);
+
+    expect(fetch).toHaveBeenCalled();
+    expect(result).toBe('');
+  });
+});
