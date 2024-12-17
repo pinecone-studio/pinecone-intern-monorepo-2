b12c92fc9229db4e1128a897729de83684bdb386
satsuralala <satsuraltumurbat@gmail.com>
2024-12-17 19:51:57 +0800

    feat: fixed complexity

--- apps/1CD/tinder/backend/src/utils/token.ts
+++ apps/1CD/tinder/backend/src/utils/token.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { NextRequest } from 'next/server';
 export const getUserId = (req: NextRequest) => {
-  const secretKey = process.env.TOKEN_SECRET || '';
+  const secretKey = `${process.env.TOKEN_SECRET}`;
   const authHeader = `${req.headers.get('Authorization')}`;
   const authToken = authHeader?.split(' ')[1];
   const isDev = process.env.NODE_ENV === 'development';
