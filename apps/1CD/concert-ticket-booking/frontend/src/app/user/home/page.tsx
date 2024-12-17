ea7cc315cb840b380289c1c71933376adea38d25
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-16 20:11:47 -0800

    feat(concert-frontend): link added

--- apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
@@ -5,6 +5,7 @@ import CardTicket from '@/components/Card';
 import { Event, useGetEventsLazyQuery } from '@/generated';
 import { useEffect } from 'react';
 import { useDebounce } from '@uidotdev/usehooks';
+import Link from 'next/link';
 
 const Page = () => {
   const [q] = useQueryState('q', { defaultValue: '' });
@@ -28,7 +29,13 @@ const Page = () => {
       <div className=" py-4  xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
         {loading && <div className="flex w-full h-full justify-center items-center">Loading...</div>}
         {data?.getEvents?.map((event) => (
-          <div key={event?._id}>{event && <CardTicket event={event as Event} />}</div>
+          <div key={event?._id}>
+            {event && (
+              <Link href={`/user/home/event/${event._id}`}>
+                <CardTicket event={event as Event} />
+              </Link>
+            )}
+          </div>
         ))}
       </div>
     </div>
