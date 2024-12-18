eb0069ba9072787c2d940f332771fbb41e8b49a4
24LP2640 24LP2640 <24LP2640@administrators-iMac.local>
2024-12-18 16:37:53 +0800

    request successfully informs the message

--- apps/1CD/leave-request/frontend/src/app/(main)/createNewRequest/page.tsx
+++ apps/1CD/leave-request/frontend/src/app/(main)/createNewRequest/page.tsx
@@ -1,9 +1,14 @@
 import { getEmail } from '@/utils/get-email';
 import { CreateNewRequest } from './CreateNewRequest';
+import { MessageContextWrapper } from '@/context/MessageContext';
 
 const Page = async () => {
-  const email = await getEmail()
-  return <CreateNewRequest email={email}/>;
+  const email = await getEmail();
+  return (
+    <MessageContextWrapper>
+      <CreateNewRequest email={email} />
+    </MessageContextWrapper>
+  );
 };
 
 export default Page;
