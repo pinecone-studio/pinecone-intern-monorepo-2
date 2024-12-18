d1a615efe90f5fe3aad283e9242bcfdfec6cc3b5
Erdenejargal Odonkhuu <erdenejargal@Erdenejargals-MacBook-Pro.local>
2024-12-18 22:56:10 +0800

    feat(concert-frontend): carousel

--- apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
@@ -2,6 +2,7 @@
 
 import { useQueryState } from 'nuqs';
 import CardTicket from '@/components/Card';
+import CarouselMain from '@/components/carousel';
 import { Event, useGetEventsLazyQuery } from '@/generated';
 import { useEffect } from 'react';
 import { useDebounce } from '@uidotdev/usehooks';
@@ -25,9 +26,9 @@ const Page = () => {
   }, [debouncedQ]);
 
   return (
-    <div className="w-full   bg-black py-10" data-cy="Home-Page">
+    <div className="w-full pt-10 bg-black" data-cy="Home-Page">
       <div className=" py-4  xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
-        {loading && <div className="flex w-full h-full justify-center items-center">Loading...</div>}
+        {loading && <div className="flex items-center justify-center w-full h-full">Loading...</div>}
         {data?.getEvents?.map((event) => (
           <div key={event?._id}>
             {event && (
