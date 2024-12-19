925a960b8dfb5f6de72ae4ca5737595109775956
Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>
2024-12-19 14:34:33 +0800

    feat: diejd

--- apps/1CD/hotel-booking/backend/specs/resolvers/queries/bookings/get-bookings.spec.ts
+++ apps/1CD/hotel-booking/backend/specs/resolvers/queries/bookings/get-bookings.spec.ts
@@ -4,18 +4,16 @@ import { getBookings } from 'src/resolvers/queries';
 
 jest.mock('src/models', () => ({
   bookingModel: {
-    find: jest.fn().mockReturnValueOnce({
-      populate: jest.fn().mockResolvedValue([
-        {
-          _id: '674ad1f265ca6d2c473739b7',
-          userId: '1',
-        },
-      ]),
-    }),
+    find: jest.fn().mockReturnValueOnce([
+      {
+        _id: '674ad1f265ca6d2c473739b7',
+        userId: '1',
+      },
+    ]),
   },
 }));
 
-describe('get bookings', () => {
+describe('getBookings function', () => {
   it('should return bookings', async () => {
     const result = await getBookings!(
       {},
@@ -32,5 +30,12 @@ describe('get bookings', () => {
         userId: '1',
       },
     ]);
+  });
+  it('if bookings not found', async () => {
+    try {
+      await getBookings!({}, {}, { userId: '1' }, {} as GraphQLResolveInfo);
+    } catch (error) {
+      expect((error as Error).message).toEqual('Bookings not found');
+    }
   });
 });
--- apps/1CD/hotel-booking/backend/src/resolvers/queries/bookings/get-bookings.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/bookings/get-bookings.ts
@@ -3,7 +3,7 @@ import { bookingModel } from 'src/models';
 
 export const getBookings: QueryResolvers['getBookings'] = async () => {
   try {
-    const bookings = await bookingModel.find({});
+    const bookings = await bookingModel.find();
     if (!bookings) throw new Error('Bookings not found');
     return bookings;
   } catch (error) {
