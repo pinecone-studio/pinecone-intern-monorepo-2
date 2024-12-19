2a87b0bea9a9c5107f413d0778b0aa274c717543
Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>
2024-12-19 15:21:33 +0800

    feat: idk

--- apps/1CD/hotel-booking/backend/src/resolvers/queries/hotels/get-hotels.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/hotels/get-hotels.ts
@@ -2,7 +2,7 @@ import { QueryResolvers } from 'src/generated';
 import { hotelsModel } from 'src/models';
 
 export const getHotels: QueryResolvers['getHotels'] = async () => {
-  const hotels = await hotelsModel.find().populate({ path: 'roomId', populate: { path: 'hotelId' } });
+  const hotels = await hotelsModel.find();
   if (!hotels.length) {
     throw new Error('Hotels not found');
   }
