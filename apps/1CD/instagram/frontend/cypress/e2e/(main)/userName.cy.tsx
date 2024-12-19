89cf4c143e4b46c0d9591a197d4209655ca21445
Erdenesuren Batbaatar <erdenesuren.bat2024@gmail.com>
2024-12-19 09:58:16 +0800

    feat(fe): user profiliin zurag oruulahtest

--- apps/1CD/instagram/frontend/cypress/e2e/(main)/userName.cy.tsx
+++ apps/1CD/instagram/frontend/cypress/e2e/(main)/userName.cy.tsx
@@ -1,3 +1,66 @@
+// describe('user profile page', () => {
+//   beforeEach(() => {
+//     cy.visit('/home/erdek');
+//   });
+
+//   it('1. Should render user profile page', () => {
+//     cy.get('[data-cy="user-profile-page"]').should('be.visible');
+//   });
+
+//   it('2. Should display user information', () => {
+//     cy.intercept('GET', 'api/gra[hql', (req) => {
+//       if (req.body.operationName === 'GetUser') {
+//         req.reply({
+//           statusCode: 200,
+//           body: { data: { User: { userName: 'mockUserName', fullName: 'Mock User', followerNumber: '99', followingNumber: '199', profileImg: 'http://example.com/profileImage1.jpg' } } },
+//         });
+//       }
+//     }).as('GetUser');
+//     cy.visit('/home/erdek');
+//   });
+
+//   it('3. Should display posts when fetch post successfully', () => {
+//     cy.intercept('GET', 'api/graphql', (req) => {
+//       if (req.body.operationName === 'getMyPosts') {
+//         req.reply({
+//           statusCode: 200,
+//           body: {
+//             data: {
+//               MyPosts: [{ image: ['http://example.com/image11.jpg', 'http://example.com/image12.jpg'], description: 'zuragtai example post' }],
+//             },
+//           },
+//         });
+//       }
+//     }).as('GetMyPostsQuery');
+//     cy.visit('/home/erdek');
+//   });
+
+//   it('4. Should display no posts infos when something wrong', () => {
+//     cy.intercept('GET', 'api/graphql', (req) => {
+//       if (req.body.operationName === 'getMyPosts') {
+//         req.reply({ statusCode: 400, body: { errors: { message: 'Something wrong' } } });
+//       }
+//     }).as('GetMyPostsQuery');
+//     cy.visit('/home/erdek');
+//     // cy.wait('@getMyPosts');
+
+//     cy.get('[data-cy="postnumberError"]').contains('Something wrong').should('be.visible');
+//     cy.get('[data-cy="postsError"]').contains('Something wrong').should('be.visible');
+//   });
+//   it('5. Should handle image then upload and save data', () => {
+//     cy.intercept('POST', 'https://api.cloudinary.com/v1_1/dka8klbhn/image/upload', (req) => {
+//       if (req.body.operationName === 'changeProfileImg') {
+//         req.reply({ statusCode: 200, body: { secure_url: 'http://example.com/profileImage11.jpg' } });
+//       }
+//     }).as('changeProfileImg');
+//     // cy.get('[data-cy="image-upload-input"]').selectFile('/Users/24LP7729/Desktop/pinecone-intern-1CD-monorepo/apps/1CD/instagram/frontend/public/images/Logo.png');
+
+//     cy.visit('/home/erdek');
+//     // cy.wait('@changeProfileImg');
+//     // cy.get('[data-cy="pro-image"]').should('have.attr', 'src', 'http://example.com/profileImage11.jpg');
+//   });
+// });
+
 describe('user profile page', () => {
   beforeEach(() => {
     cy.intercept('GET', 'api/graphql', (req) => {
@@ -29,16 +92,17 @@ describe('user profile page', () => {
     cy.get('[data-cy="user-profile-page"]').should('be.visible');
   });
 
-  it.only('2. Should display user information', () => {
-    cy.get('[data-cy="pro-image"]').should('be.visible', 'http://example.com/profileImage1.jpg');
-    cy.get('[data-cy="username"]').should('contain', 'mockUserName');
-    cy.get('[data-cy="followerNumber"]').should('contain', 99);
-    cy.get('[data-cy="followingNumber"]').should('contain', 199);
-    cy.get('[data-cy="fullname"]').should('contain.text', 'Mock User');
-  });
+  // it.only('2. Should display user information', () => {
+  //   cy.get('[data-cy="pro-image"]').should('have.attr', 'src', 'http://example.com/profileImage1.jpg');
+  //   cy.get('[data-cy="username"]').should('contain.text', 'erdek');
+  //   cy.get('[data-cy="followerNumber"]').should('contain', 99);
+  //   cy.get('[data-cy="followingNumber"]').should('contain', 199);
+  //   cy.get('[data-cy="fullname"]').should('contain.text', 'Mock User');
+  // });
 
-  it('3. Should display posts when fetch post successfully', () => {
+  it.only('2. Should display posts when fetch post successfully', () => {
     cy.visit('/home/erdek');
+    cy.get('[data-cy="postNumberDone"]').should('contain', 1);
   });
 
   it('4. Should display no posts infos when something wrong', () => {
