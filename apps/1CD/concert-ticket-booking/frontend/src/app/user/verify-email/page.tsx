163f26d831613dc93479a8dcf0086c593d4e6fae
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-12-20 18:03:05 +0800

    feat(concert-frontend): verify user email

---
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/verify-email/page.tsx
@@ -0,0 +1,12 @@
+import VerifyEmail from '@/app/user/verify-email/_components/VerifyEmail';
+import React from 'react';
+
+const VerifyEmailPage = () => {
+  return (
+    <div>
+      <VerifyEmail />
+    </div>
+  );
+};
+
+export default VerifyEmailPage;
--- apps/1CD/concert-ticket-booking/frontend/src/components/auth/SignUp.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/components/auth/SignUp.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
 import { useAuth } from '@/components/providers/AuthProvider';
 import { z } from 'zod';
-import LoadingSpinner from './loading/Loading';
+import LoadingSpinner from '../loading/Loading';
 
 const inputs = [
   {
@@ -114,6 +114,11 @@ const SignUp = () => {
               хэсгээр <br />
               орно уу.
             </p>
+            <button data-cy="Verify-Email-Link-Button" data-testid="Verify-Email-Link-Button" className="text-xs text-center text-zinc-400">
+              <Link href="/user/verify-email" className="mx-1 underline underline-offset-2 decoration-white hover:text-white ">
+                Нууц үг сэргээх
+              </Link>
+            </button>
           </div>
         </form>
       </Form>
