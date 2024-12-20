55c34e907e30b177ee0181cd684f7776ac005946
24LP2640 24LP2640 <24LP2640@administrators-iMac.local>
2024-12-20 15:22:57 +0800

    set token on render all pages

--- apps/1CD/leave-request/frontend/cypress/e2e/render-all.pages.cy.tsx
+++ apps/1CD/leave-request/frontend/cypress/e2e/render-all.pages.cy.tsx
@@ -1,6 +1,11 @@
 import allPages from '../utils/all-pages.json';
 
 describe('render all pages', () => {
+  const token = Cypress.env().env['ANNUAL_TOKEN'] as string
+    cy.setCookie(
+      'authtoken',
+      token
+    );
   it(`Should render all page`, () => {
     cy.log(JSON.stringify(allPages));
     allPages.forEach((page) => {
