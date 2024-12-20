68625fd44925254716c2cc2ddb47851adc54f33f
24LP2640 24LP2640 <24LP2640@administrators-iMac.local>
2024-12-20 15:37:21 +0800

    fix test

--- apps/1CD/leave-request/frontend/cypress/e2e/render-all.pages.cy.tsx
+++ apps/1CD/leave-request/frontend/cypress/e2e/render-all.pages.cy.tsx
@@ -1,11 +1,13 @@
 import allPages from '../utils/all-pages.json';
 
 describe('render all pages', () => {
-  const token = Cypress.env().env['ANNUAL_TOKEN'] as string
+  beforeEach(() => {
+    const token = Cypress.env().env['ANNUAL_TOKEN'] as string
     cy.setCookie(
       'authtoken',
       token
     );
+  });
   it(`Should render all page`, () => {
     cy.log(JSON.stringify(allPages));
     allPages.forEach((page) => {
