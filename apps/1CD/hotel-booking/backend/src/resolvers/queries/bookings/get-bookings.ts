ba0d29921a3dc8489fa20a8baa137f5af1670a77
Dwjrgal <dwjrgl651@gmail.com>
2024-12-20 20:14:05 +0800

    feat: bookings query and unit test (#696)
    Co-authored-by: Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>

--- apps/1CD/hotel-booking/backend/src/resolvers/queries/bookings/get-bookings.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/bookings/get-bookings.ts
@@ -1,10 +1,20 @@
-import { QueryResolvers } from 'src/generated';
+import { QueryResolvers, BookingsType } from 'src/generated';
 import { bookingModel } from 'src/models';
 
 export const getBookings: QueryResolvers['getBookings'] = async () => {
   try {
-    const bookings = await bookingModel.find();
-    if (!bookings) throw new Error('Bookings not found');
+    const bookings: BookingsType[] = await bookingModel
+      .find()
+      .populate({ path: 'userId' })
+      .populate({
+        path: 'roomId',
+        populate: { path: 'hotelId' },
+      });
+
+    if (!bookings || bookings.length === 0) {
+      throw new Error('No bookings found');
+    }
+
     return bookings;
   } catch (error) {
     throw new Error((error as Error).message);
