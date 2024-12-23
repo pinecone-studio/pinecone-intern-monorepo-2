d2aacc7300e6b476f7b76e0cff327c7a8a0462f7
Anarchuluu Bayarsaikhan <anarchulu@gmail.com>
2024-12-23 13:22:56 +0800

    719 tinderchat backend merging (#723)
    * feat(tinder chat schema typedefs adding): tinder chat schema typedefs adding
    
    * feat(tinder chat backend with unit test done): tinder chat backend with unit test done
    
    * feat(tinder backend): tinder chat adding
    
    * feat: tinder chat
    
    * feat(tinder chat): tinder chat frontend and backend
    
    * feat([tinder]): tinder chat
    
    * feat([tinder]): tinder frontend backend
    
    * feat([tinder]): tinder frontend backend
    
    * feat([tinder]): tinder backend and frontend
    
    * feat([tinder]): tinder chat frontend and backend
    
    * Remove yarn.lock
    
    * feat(tinder): conflict fixing
    
    * feat(tinder  backend): tinder layout bug fixed to merge backend
    
    * Your commit message
    
    * feat(ji): package.json added
    
    * feat(tinder): bug fix
    
    * feat(tinder): bug fix
    
    * feat(tinder): removed cypress test to merge backend first
    
    ---------
    
    Co-authored-by: Anarchuluu Bayarsaikhan <anarchulu@Anarchuluus-MacBook-Air.local>

--- apps/1CD/tinder/backend/src/schemas/index.ts
+++ apps/1CD/tinder/backend/src/schemas/index.ts
@@ -1,5 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
 import { typeDefs as UserTypeDefs } from "./user/user.schema";
+import { typeDefs as TinderchatTypeDefs } from "./tinderchat/chat.schema";
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs,UserTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs,UserTypeDefs,TinderchatTypeDefs]);
