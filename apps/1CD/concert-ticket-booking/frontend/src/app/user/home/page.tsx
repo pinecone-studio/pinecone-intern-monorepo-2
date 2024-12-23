908dfa335be0cf6580bf95e7c830de863c33028c
erkabb <erkabb816@gmail.com>
2024-12-23 17:14:23 +0800

    333 concert ticket frontend main page carousel (#745)
    * feat(concert-frontend): carousel
    
    * feat(concert-frontend): resolve
    
    * feat(concert-frontend): carousel test
    
    * test carousel
    
    * feat(concert-frontend): test
    
    ---------
    
    Co-authored-by: Erdenejargal Odonkhuu <erdenejargal@Erdenejargals-MacBook-Pro.local>

--- apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
@@ -2,41 +2,34 @@
 
 import { useQueryState } from 'nuqs';
 import CardTicket from '@/components/Card';
-import { Event, useGetEventsLazyQuery } from '@/generated';
-import { useEffect, useState } from 'react';
+import CarouselMain from '@/app/user/home/_components';
+import { Event, useGetEventsLazyQuery, useGetSpecialEventQuery } from '@/generated';
+import { useEffect } from 'react';
 import { useDebounce } from '@uidotdev/usehooks';
 import Link from 'next/link';
-import { Input } from '@/components/ui/input';
-import { IoSearch } from 'react-icons/io5';
 
 const Page = () => {
   const [q] = useQueryState('q', { defaultValue: '' });
-  const [artist, setArtist] = useState('');
-
   const debouncedQ = useDebounce(q, 300);
-
   const [getEvents1, { data, loading }] = useGetEventsLazyQuery();
+  const { data: eventData } = useGetSpecialEventQuery();
+  const firstEvent = eventData?.getSpecialEvent;
 
   useEffect(() => {
     getEvents1({
       variables: {
         filter: {
           q: debouncedQ,
-          artist: artist,
         },
       },
     });
-  }, [debouncedQ, artist]);
+  }, [debouncedQ]);
 
   return (
-    <div className="w-full   bg-black py-10" data-cy="Home-Page">
+    <div className="w-full pt-10 bg-black" data-cy="Home-Page">
+      {firstEvent && <CarouselMain event={firstEvent} />}
       <div className=" py-4  xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
-        <div className="relative flex items-center w-[200px] px-2 md:px-6 md:w-auto text-white">
-          <Input data-testid="Search-Input" type="text" placeholder=" Хайлт" className=" w-full bg-black border-gray-600 md:w-80" value={artist} onChange={(e) => setArtist(e.target.value)} />
-        </div>
-        <div></div>
-        <div className="hidden xl:block"></div>
-        {loading && <div className="flex w-full h-full justify-center items-center">Loading...</div>}
+        {loading && <div className="flex items-center justify-center w-full h-full">Loading...</div>}
         {data?.getEvents?.map((event) => (
           <div key={event?._id}>
             {event && (
