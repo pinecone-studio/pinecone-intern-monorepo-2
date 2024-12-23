463ad7408e42fab8a650ca8028d1c49b6b760051
Erdenejargal Odonkhuu <erdenejargal@Erdenejargals-MacBook-Pro.local>
2024-12-23 16:38:58 +0800

    test carousel

--- apps/1CD/concert-ticket-booking/frontend/cypress/e2e/user/home/index.cy.tsx
+++ apps/1CD/concert-ticket-booking/frontend/cypress/e2e/user/home/index.cy.tsx
@@ -7,18 +7,8 @@ describe('Home Page', () => {
     cy.get('[data-cy="Card-Component"]').should('be.visible');
   });
 
-  // it('should render carousel items correctly', () => {
-  //   // Verify that the carousel container is visible
-  //   cy.get('[data-cy="events"]').should('be.visible');
-
-  //   // Verify that the first event is displayed
-  //   cy.get('[data-cy="eventId"]').within(() => {
-  //     cy.contains('Event 1').should('be.visible');
-  //   });
-  // });
-
   it('should navigate to the next item on clicking the next button', () => {
-    // Click the next button
+
     cy.get('[data-cy="next-button"]').click();
 
     cy.get('[data-cy="prev-button"]').click();
