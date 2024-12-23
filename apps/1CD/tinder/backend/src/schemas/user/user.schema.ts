fbaa18f7fbbf54ef7bf5075abf97a41cf577bc26
satsuralala <satsuraltumurbat@gmail.com>
2024-12-23 17:36:57 +0800

    feat: tinder:users query

--- apps/1CD/tinder/backend/src/schemas/user/user.schema.ts
+++ apps/1CD/tinder/backend/src/schemas/user/user.schema.ts
@@ -4,18 +4,18 @@ export const typeDefs = gql`
   scalar Date
   type User {
     _id: ID!
-    name: String!
-    email: String!
-    bio: String!
-    age: Int!
-    gender: String!
-    interests: [String!]
-    photos: [String!]
-    profession: String!
-    schoolWork: [String!]
-    createdAt: Date!
-    updatedAt: Date!
-    attraction: String!
+    name: String
+    email: String
+    bio: String
+    age: Int
+    gender: String
+    interests: [String]
+    photos: [String]
+    profession: String
+    schoolWork: [String]
+    createdAt: Date
+    updatedAt: Date
+    attraction: String
   }
 
   input RegisterEmailInput {
