e61b799c4db75f3c2a2db9fdab0db3930120c7d2
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-12-23 19:42:08 +0800

    request are grouped by request date and sorted in descending order (#762)
    * request are grouped by request date and sorted in descending order
    
    * sorted requests
    
    ---------
    
    Co-authored-by: 24LP2640 24LP2640 <24LP2640@administrators-iMac.local>

--- apps/1CD/leave-request/backend/src/resolvers/queries/request/get-all-request.ts
+++ apps/1CD/leave-request/backend/src/resolvers/queries/request/get-all-request.ts
@@ -1,18 +1,32 @@
-import { QueryResolvers } from "src/generated";
-import { RequestModel } from "src/models";
+import { QueryResolvers } from 'src/generated';
+import { RequestModel } from 'src/models';
 
-export const getRequests: QueryResolvers['getRequests'] = async (_, {email, startDate, endDate, status }) => {
-    
-    let query = {}
-    if (email){
-        query = {...query, email}
-    }
-    if(startDate){
-        query = {...query, requestDate: {$gte: startDate, $lte: endDate}}
-    }
-    if(status){
-        query = {...query, result: status}
-    }
-    const requests = await RequestModel.find(query)
-    return requests
-}
+export const getRequests: QueryResolvers['getRequests'] = async (_, { email, startDate, endDate, status }) => {
+  const matchQuery = calculateFilter(email, startDate, endDate, status);
+  const groupedRequests = await RequestModel.aggregate([
+    { $match: matchQuery },
+    {
+      $group: {
+        _id: { $dateToString: { format: '%Y-%m-%d', date: '$requestDate' } },
+        requests: { $push: '$$ROOT' },
+      },
+    },
+    { $sort: { _id: -1 } },
+  ]);
+
+  return groupedRequests;
+};
+
+const calculateFilter = (email?: string, startDate?: Date, endDate?: Date, status?: string) => {
+  const matchQuery: any = {};
+  if (email) {
+    matchQuery.email = email;
+  }
+  if (startDate) {
+    matchQuery.requestDate = { $gte: startDate, $lte: endDate };
+  }
+  if (status) {
+    matchQuery.result = status;
+  }
+  return matchQuery;
+};
