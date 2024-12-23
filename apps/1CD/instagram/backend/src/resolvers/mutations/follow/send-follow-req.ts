a6fd110650a145d57a34c0035fb57aaa8f88f08a
Myeruyert Janibyek <24LP4972@admins-iMac.local>
2024-12-23 04:00:52 -0800

    edit send-follow-req-mutation

--- apps/1CD/instagram/backend/src/resolvers/mutations/follow/send-follow-req.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/follow/send-follow-req.ts
@@ -2,10 +2,16 @@ import { AccountVisibility, FollowStatus, MutationResolvers } from '../../../gen
 import { followModel } from '../../../models/follow.model';
 import { userModel } from '../../../models/user.model';
 
-export const sendFollowReq: MutationResolvers['sendFollowReq'] = async (_: unknown, { followerId, followingId }) => {
+export const sendFollowReq: MutationResolvers['sendFollowReq'] = async (_: unknown, { followerId, followingId }, { userId }) => {
+  if (followerId !== userId) {
+    throw new Error('Unauthorized');
+  }
+
   const user = await userModel.findById(followingId);
 
-  if (!user) throw new Error('User not found');
+  if (!user) {
+    throw new Error('User not found');
+  }
 
   const { accountVisibility } = user;
 
