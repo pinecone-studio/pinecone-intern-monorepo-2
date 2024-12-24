f7b3f8d67f114751e2b8a2d66d7c78f9552b96bf
Nomt <nomttumurkhuu@yahoo.com>
2024-12-24 12:06:14 +0800

    hotel detail test uurchluv

--- apps/1CD/hotel-booking/frontend/src/app/(public)/layout.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(public)/layout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 import '.././global.css';
 import FooterHome from '@/components/FooterHome';
 import Header from '@/components/Header';
-import HotelDetail from '../(client)/hotel-detail/HotelDetail';
+import HotelDetail from '../(client)/_components/HotelDetail';
 
 const PublicLayout = ({ children }: PropsWithChildren) => {
   return (
