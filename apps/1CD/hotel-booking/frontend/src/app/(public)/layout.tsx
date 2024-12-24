a741f265fe752f6325f501e21cc61b6646f8eab8
Nomt <nomttumurkhuu@yahoo.com>
2024-12-24 11:40:32 +0800

    Hoteldetail component layout dotor nemev

--- apps/1CD/hotel-booking/frontend/src/app/(public)/layout.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(public)/layout.tsx
@@ -2,11 +2,13 @@ import { PropsWithChildren } from 'react';
 import '.././global.css';
 import FooterHome from '@/components/FooterHome';
 import Header from '@/components/Header';
+import HotelDetail from '../(client)/hotel-detail/HotelDetail';
 
 const PublicLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <Header />
+      <HotelDetail />
       {children}
       <FooterHome />
     </>
