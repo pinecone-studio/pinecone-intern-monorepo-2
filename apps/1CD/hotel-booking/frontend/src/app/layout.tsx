fd06a2112ac757024d7426b8a132fafacd25ca49
Nomt <nomttumurkhuu@yahoo.com>
2024-12-24 12:36:25 +0800

    folder structure ynzlav layout uusgej (#773)
    * folder structure ynzlav layout uusgej
    
    * Hoteldetail component layout dotor nemev
    
    * hotel detail test uurchluv

--- apps/1CD/hotel-booking/frontend/src/app/layout.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/layout.tsx
@@ -1,24 +1,18 @@
 import { PropsWithChildren } from 'react';
 import './global.css';
 import { ApolloWrapper } from '@/components/providers';
-import FooterHome from '@/components/FooterHome';
-import Header from '@/components/Header';
-import HotelDetail from './(client)/hotel-detail/HotelDetail';
 import { ToastContainer } from 'react-toastify';
 
 const RootLayout = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
-      <body>
-        <ApolloWrapper>
-          <Header />
-          <HotelDetail />
+    <ApolloWrapper>
+      <html lang="en">
+        <body>
           {children}
-          <FooterHome />
           <ToastContainer />
-        </ApolloWrapper>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ApolloWrapper>
   );
 };
 
