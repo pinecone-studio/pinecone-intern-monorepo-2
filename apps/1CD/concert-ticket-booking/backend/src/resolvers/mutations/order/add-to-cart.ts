efdfab38f8f9c4d39f350f242c9b37fe0e294dd0
Abylai12 <abilay1208@gmail.com>
2024-12-25 14:18:57 +0800

    fix(concert-backend): to improve order ticket mutation (#815)
    Co-authored-by: Abylai12 <abylai@Aigyerims-MacBook-Air.local>

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/order/add-to-cart.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/order/add-to-cart.ts
@@ -1,27 +1,31 @@
-import { MutationResolvers, MyTicketTypeInput } from "../../../generated"; 
-import Order from "../../../models/order.model";
-import Ticket from "../../../models/ticket.model"; 
+import { MutationResolvers, TicketType } from '../../../generated';
+import Order from '../../../models/order.model';
+import Ticket from '../../../models/ticket.model';
 
-    export const addToCarts: MutationResolvers['addToCarts'] = async (_, { input }, { userId }) => {
-         const { ticketId, ticketType } = input; 
-         const findTicket = await Ticket.findById(ticketId); 
-         if (findTicket) { 
-            ticketType.forEach(({ _id, soldQuantity }) => { 
-            
-            const ticketIdx = findTicket.ticketType.findIndex( (item: MyTicketTypeInput) => item._id.toString() === _id.toString() ); 
-            
-            if (ticketIdx > -1) { 
-                if(findTicket.ticketType[ticketIdx].soldQuantity + soldQuantity < findTicket.ticketType[ticketIdx].totalQuantity){ findTicket.ticketType[ticketIdx].soldQuantity += soldQuantity; 
-
-                }else { 
-                    throw new Error('Seats are full'); 
-                } 
-            } 
-        }); 
-    } 
-    await findTicket.save(); 
-    const createOrder = await Order.create({ userId, ...input, 
-
-    }); 
-    return createOrder 
+export const addToCarts: MutationResolvers['addToCarts'] = async (_, { input }, { userId }) => {
+  if (!userId) throw new Error('Unauthorized');
+  const { ticketId, ticketType, eventId, phoneNumber, email } = input;
+  const findTicket = await Ticket.findById(ticketId);
+  if (findTicket) {
+    ticketType.forEach(({ _id, buyQuantity }) => {
+      const ticketIdx = findTicket.ticketType.findIndex((item: TicketType) => item._id.toString() === _id.toString());
+      if (ticketIdx > -1) {
+        if (findTicket.ticketType[ticketIdx].soldQuantity + Number(buyQuantity) < findTicket.ticketType[ticketIdx].totalQuantity) {
+          findTicket.ticketType[ticketIdx].soldQuantity += Number(buyQuantity);
+        } else {
+          throw new Error('Seats are full');
+        }
+      }
+    });
+  }
+  await findTicket.save();
+  const updatedTicketTypes = ticketType.map((item) => {
+    const matchedTicket = findTicket.ticketType.find((x: TicketType) => x._id.toString() === item._id.toString());
+    if (matchedTicket) {
+      matchedTicket.soldQuantity = item.buyQuantity;
+      return matchedTicket;
+    }
+  });
+  await Order.create({ userId, ticketId, eventId, phoneNumber, email, ticketType: updatedTicketTypes });
+  return { message: 'success' };
 };
