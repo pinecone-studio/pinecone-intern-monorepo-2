ca8b0409af9444f0d5be9d943949f70d872f5603
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-26 15:16:47 +0800

    feat(concert-backend): filter-backend

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/queries/event/get-events.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/queries/event/get-events.ts
@@ -6,7 +6,6 @@ export const getEvents: QueryResolvers['getEvents'] = async (_, { filter = {} })
   const today = new Date().toISOString();
   const { q, date, artist } = filter;
   const findFilter: any = { $and: [{ scheduledDays: { $elemMatch: { $gte: today } } }] };
-  const dateSplitted = date?.split('T')[0];
 
   if (q) {
     findFilter.$and.push({
@@ -22,16 +21,28 @@ export const getEvents: QueryResolvers['getEvents'] = async (_, { filter = {} })
   }
 
   if (date) {
-    const startDate = dateSplitted + 'T00:00';
-    const endDate = dateSplitted + 'T23:59';
-    findFilter.$and.push({ scheduledDays: { $elemMatch: { $gte: startDate, $lte: endDate } } });
+    const startIsoDate = date;
+    const startDate = new Date(startIsoDate);
+    startDate.setHours(23);
+    startDate.setMinutes(59);
+    const endIsoDate = startDate.toISOString();
+
+    findFilter.$and.push({
+      scheduledDays: {
+        $elemMatch: {
+          $gte: startIsoDate,
+          $lte: endIsoDate,
+        },
+      },
+    });
   }
 
   if (artist) {
-    findFilter.$and.push({ $or: [{ mainArtists: { $elemMatch: { $regex: new RegExp(artist, 'i') } } }, { guestArtists: { $elemMatch: { $regex: new RegExp(artist, 'i') } } }] });
+    findFilter.$and.push({
+      $or: [{ mainArtists: { $elemMatch: { name: { $regex: new RegExp(artist, 'i') } } } }, { guestArtists: { $elemMatch: { name: { $regex: new RegExp(artist, 'i') } } } }],
+    });
   }
 
   const events: EventType[] = await Event.find(findFilter).populate(['products', 'venue']);
-
   return events;
 };
