86f5e7c156068f616096b58dc45f7fc138e735b5
24LP9968 24LP9968 <24LP9968@administrators-iMac.local>
2024-12-26 18:17:13 +0800

    feat: tinder user schema update

--- apps/1CD/tinder/frontend/src/app/(main)/sign-up/image/page.tsx
+++ apps/1CD/tinder/frontend/src/app/(main)/sign-up/image/page.tsx
@@ -15,8 +15,6 @@ const ImageUpload = () => {
     router.push('/');
   };
 
-
-  
   return (
     <div className="mx-auto flex justify-center w-full max-w-4xl">
       <div className="flex flex-col items-center w-full">
@@ -29,7 +27,7 @@ const ImageUpload = () => {
           Upload your image
         </p>
         <p className="text-[#71717A] text-sm" data-cy="question-description">
-          Please choose an image that represents you.
+          Please choose an image that represents you
         </p>
         <div className="grid grid-rows-2 grid-cols-3 gap-6 mt-[24px]">
           <div
