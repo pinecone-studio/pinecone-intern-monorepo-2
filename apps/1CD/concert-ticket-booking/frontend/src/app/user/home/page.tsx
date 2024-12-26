8e1cc009fde31e01a9402b39ee1e4cfbe9ced2de
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-12-26 17:41:27 +0800

    feat(concert-frontend): header-responsive

--- apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/user/home/page.tsx
@@ -9,7 +9,6 @@ import Link from 'next/link';
 
 const Page = () => {
   const [q] = useQueryState('q', { defaultValue: '' });
-
   const debouncedQ = useDebounce(q, 300);
   const [getEvents1, { data, loading }] = useGetEventsLazyQuery();
   const { data: eventData } = useGetSpecialEventQuery();
@@ -26,9 +25,9 @@ const Page = () => {
   }, [debouncedQ]);
 
   return (
-    <div className="w-full pt-10 bg-black" data-cy="Home-Page">
+    <div className="w-full  bg-black" data-cy="Home-Page">
       {firstEvent && <CarouselMain event={firstEvent} />}
-      <div className=" py-4  xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
+      <div className=" py-4 pt-12 xl:w-[1100px] md:w-[700px] w-[350px] mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-4 ">
         {loading && <div className="flex items-center justify-center w-full h-full">Loading...</div>}
         {data?.getEvents?.map((event) => (
           <div key={event?._id}>
