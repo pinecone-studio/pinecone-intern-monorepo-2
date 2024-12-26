dfb2fca6cd9835ccc7bc01ef4526cb82305bfd64
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2024-12-26 17:15:16 +0800

    get post like IG team

--- apps/1CD/instagram/backend/src/resolvers/queries/post-like/get-post-likes.ts
+++ apps/1CD/instagram/backend/src/resolvers/queries/post-like/get-post-likes.ts
@@ -4,5 +4,6 @@ import { QueryResolvers } from '../../../generated';
 export const getPostLikes: QueryResolvers['getPostLikes'] = async (_, { postId }, { userId }) => {
   if (!userId) throw new Error('Unauthorized');
   const postLikes = await PostLikeModel.find({ post: postId });
+  if (!postLikes) throw new Error('This post have not a like');
   return postLikes;
 };
