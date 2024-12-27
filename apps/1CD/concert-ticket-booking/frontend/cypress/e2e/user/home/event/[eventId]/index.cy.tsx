9d5c5185e4825cb2fd6e711606b09e770318bf93
Tuul <tuul@Tuuls-MacBook-Air.local>
2024-12-27 18:05:26 +0800

    feat(detail page): related events edit

--- apps/1CD/concert-ticket-booking/frontend/cypress/e2e/user/home/event/[eventId]/index.cy.tsx
+++ apps/1CD/concert-ticket-booking/frontend/cypress/e2e/user/home/event/[eventId]/index.cy.tsx
@@ -4,7 +4,7 @@ import { OneMockEvent } from 'cypress/utils/one-mock-event';
 
 describe('Detail Page', () => {
   beforeEach(() => {
-    cy.visit('user/home/event/[eventId]'); // Event ID-г URL-д тохируулж зассан
+    cy.visit('user/home/event/[eventId]');
   });
 
   it('Displays the Detail Page top component', () => {
@@ -12,40 +12,39 @@ describe('Detail Page', () => {
     cy.get('[data-cy="DetailTop-Component"]').should('be.visible'); // DetailTop component байгаа эсэхийг шалгах
   });
 
-  it('Displays the correct event details', () => {
+  it('Displays the correct event details with mockEvent', () => {
     interceptGraphql({
       state: 'success',
       operationName: 'GetRelatedEvents',
       data: {
-        data: mockEvent,
+        data: mockEvent, // Mock өгөгдлийг ашиглаж байна
       },
     });
 
     cy.get('[data-cy="Venue-Name"]').should('contain.text', 'Төв цэнгэлдэх хүрээлэн'); // Venue нэрийг шалгах
-    cy.get('[data-cy="Guest-Artist-0"]').should('contain.text', 'Jojo');
+    cy.get('[data-cy="Guest-Artist-0"]').should('contain.text', 'Jojo'); // Зочин уран бүтээлчийн нэр
     cy.get('[data-cy="select-day-button"]').click();
     cy.get('[data-cy="select-day-item"]').contains('05.31').click();
 
     cy.get('[data-cy="select-day-button"]').should('contain.text', 'Сонгосон өдөр: 05.31');
-
-    // cy.get('[data-cy="selected-products"]').should('contain.text', 'VIP').and('contain.text', '50000₮');
     cy.get('[data-cy="Scheduled-Days"]').should('be.visible');
     cy.get('[data-cy="discount-price-0"]').should('contain.text', '450000');
   });
-  it('Displays the correct event details', () => {
+
+  it('Displays the correct event details with OneMockEvent', () => {
     interceptGraphql({
-      state: '',
+      state: 'success',
       operationName: 'GetRelatedEvents',
       data: {
-        data: OneMockEvent,
+        data: OneMockEvent, // Өөр mock өгөгдлийг ашиглаж байна
       },
     });
+
     cy.get('[data-cy="Scheduled-Days"]').should('be.visible');
-    cy.get('[data-cy="Scheduled-Day-0"]').should('contain.text', '25.06.08');
-    cy.get('[data-cy="Scheduled-Days"]').should('be.visible');
-    cy.get('[data-cy="Scheduled-Days"]').should('be.visible');
+    cy.get('[data-cy="Scheduled-Day-0"]').should('contain.text', '25.06.08'); // Зөв огноо шалгах
     cy.get('[data-cy="select-day-button"]').click();
     cy.get('[data-cy="select-day-item"]').contains('06.08').click();
+    cy.get('[data-cy="select-day-button"]').should('contain.text', 'Сонгосон өдөр: 06.08');
     cy.get('[data-cy="unit-price-0"]').should('contain.text', '500000');
   });
 });
