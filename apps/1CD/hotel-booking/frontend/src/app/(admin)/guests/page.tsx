c214e0874a1fdf05855000b7f793a5643bdce343
Даваажаргал Hасантогтох <24LP9136@administrators-iMac.local>
2024-12-27 15:58:42 +0800

    feat: bookings filters

--- apps/1CD/hotel-booking/frontend/src/app/(admin)/guests/page.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(admin)/guests/page.tsx
@@ -4,16 +4,27 @@ import { SidebarTrigger } from '@/components/ui/sidebar';
 import { Input } from '@/components/ui/input';
 
 import StatusSelect from './_components/Select';
-import React, { useState } from 'react';
-import { useGetBookingsQuery } from '@/generated';
+import React, { useEffect, useState } from 'react';
+import { useGetBookingsLazyQuery } from '@/generated';
 import DataTable from './_components/DataTable';
 import BreadCrumb from './_components/BreadCrumb';
 import { Table, TableHeader, TableHead, TableRow } from '@/components/ui/table';
 
 const Page = () => {
-  const { data } = useGetBookingsQuery();
-  const [, setSearchValue] = useState('');
+  const [getBookings, { data }] = useGetBookingsLazyQuery();
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedStatus, setSelectedStatus] = useState('all');
 
+  const filteredBookings = data?.getBookings.filter((booking) => {
+    const matchesStatus = selectedStatus === 'all' || booking.status === selectedStatus;
+    const matchesSearch = booking.userId?.firstName?.toLowerCase().includes(searchValue.toLowerCase());
+    return matchesStatus && matchesSearch;
+  });
+
+  useEffect(() => {
+    getBookings();
+  }, [setSelectedStatus, getBookings]);
+  console.log('BookingsData', data?.getBookings);
   return (
     <section data-cy="Get-Bookings-Page" className="w-screen bg-gray-50">
       <div className="flex items-center gap-2 my-6 ml-5">
@@ -25,7 +36,7 @@ const Page = () => {
           <h2 className="text-2xl font-semibold">Guests</h2>
           <div className="flex gap-3" data-cy="Bookings-Filters">
             <Input className="max-w-[1400px] bg-white" data-cy="Bookings-Search-Input" placeholder="Search" onChange={(e) => setSearchValue(e.target.value)} />
-            <StatusSelect />
+            <StatusSelect setSelectedStatus={setSelectedStatus} />
           </div>
         </div>
         <Table className="max-w-[1600px] bg-white" data-cy="Bookings-Data">
@@ -40,7 +51,7 @@ const Page = () => {
               <TableHead className="flex items-center w-40 h-8 font-semibold text-black">Status</TableHead>
             </TableRow>
           </TableHeader>
-          {data?.getBookings.map((bookingsData) => (
+          {filteredBookings?.map((bookingsData) => (
             <DataTable key={bookingsData._id} bookingsData={bookingsData} />
           ))}
         </Table>
