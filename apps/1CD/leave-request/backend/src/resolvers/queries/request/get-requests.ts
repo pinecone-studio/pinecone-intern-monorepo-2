481fa8c9536b5a87c27c5569a50086aa9050f7ac
Zoljargal2004 <zolookorzoloo@gmail.com>
2024-12-27 15:03:13 +0800

    aggregame will replace populate (#893)
    Co-authored-by: 24LP2640 24LP2640 <24LP2640@administrators-iMac.local>

--- apps/1CD/leave-request/backend/src/resolvers/queries/request/get-requests.ts
+++ apps/1CD/leave-request/backend/src/resolvers/queries/request/get-requests.ts
@@ -1,9 +1,24 @@
-import { QueryResolvers } from "src/generated"
-import { RequestModel } from "src/models"
+import { QueryResolvers } from 'src/generated';
+import { RequestModel } from 'src/models';
 
+export const getAllRequestsBySupervisor: QueryResolvers['getAllRequestsBySupervisor'] = async (_, { supervisorEmail }) => {
+  const results = await RequestModel.aggregate([
+    {
+      $match: { supervisorEmail },
+    },
+    {
+      $lookup: {
+        from: 'users', // The name of the collection to join (lowercase and plural by default)
+        localField: 'email', // Field in the `Requests` collection
+        foreignField: 'email', // Field in the `Users` collection
+        as: 'email', // The output array field for the joined data
+      },
+    },
+    {
+      $unwind: '$email', // Flatten the joined data (optional, if you want a single object per document)
+    },
+  ]);
 
-export const getAllRequestsBySupervisor : QueryResolvers['getAllRequestsBySupervisor']= async (_,{supervisorEmail}) => {
-    const requests = RequestModel.find({supervisorEmail})
-    return requests
-}
-
+  // console.log(bag)
+  return results;
+};
