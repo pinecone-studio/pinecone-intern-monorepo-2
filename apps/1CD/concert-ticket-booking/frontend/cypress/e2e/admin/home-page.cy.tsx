640d552beeb339041f0939a7e059bccca466618f
Ts3nguun <0805tsenguun@gmail.com>
2024-12-27 18:06:37 +0800

    test: cypress

--- apps/1CD/concert-ticket-booking/frontend/cypress/e2e/admin/home-page.cy.tsx
+++ apps/1CD/concert-ticket-booking/frontend/cypress/e2e/admin/home-page.cy.tsx
@@ -1,16 +1,24 @@
-describe('Admin Page ', () => {
+describe('Admin Page', () => {
   beforeEach(() => {
     cy.visit('/admin/home');
   });
+
   it('should render the copyright text', () => {
     cy.get('[data-cy="Admin-Footer"]').should('be.visible');
     cy.get('[data-cy="Admin-Footer"]').should('contain.text', '©2024 Copyright');
-    cy.get('[data-cy="AdminHeader-Logo-Text"]').should('contain.text', 'TICKET BOOKING');
-    cy.get('[data-cy="Admin-Header-Exit-Account"]').click()
   });
-  it('should render cansel request page', () => {
+
+  it('should render cancel request page', () => {
     cy.visit('/admin/cancel-request');
     cy.get('[data-cy="Cancel-Request-Text"]').should('be.visible');
     cy.get('[data-cy="Cancel-Request-Text"]').should('contain.text', 'Hello Cancel request page');
+  });
+
+  it('should render header', () => {
+    cy.get('[data-cy="AdminHeader-Logo-Text"]').should('contain.text', 'TICKET BOOKING');
+    cy.get('[data-cy="AdminHeader-MenuBar-Button"]').click();
+    cy.get('[data-cy="AdminHeader-MenuBar-Button"]').should('be.visible');
+    cy.get('[data-cy="AdminHeader-MenuBar-Content-Button-One"]').click();
+    cy.get('[data-cy="AdminHeader-MenuBar-Content-Button-Two"]').click();
   });
 });
--- apps/1CD/concert-ticket-booking/frontend/src/app/admin/home/_components/AdminHeader.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/admin/home/_components/AdminHeader.tsx
@@ -2,7 +2,9 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
-import { Button } from '@/components/ui/button';
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from '@/components/ui/menubar';
+import { useAuth } from '@/components/providers';
+import { User } from 'lucide-react';
 
 const navigation = [
   {
@@ -15,7 +17,8 @@ const navigation = [
   },
 ];
 
-export const AdminHeader = ({ onExit }: { onExit?: () => void }) => {
+export const AdminHeader = () => {
+  const { user, signout } = useAuth();
   const [activeLink, setActiveLink] = useState('/home');
 
   const handleLinkClick = (link: string) => {
@@ -23,39 +26,51 @@ export const AdminHeader = ({ onExit }: { onExit?: () => void }) => {
   };
 
   const exitAccount = () => {
-    if (onExit) {
-      onExit();
-    }
+    signout();
   };
 
   return (
-    <div className="px-4 py-4 text-black bg-white max-w-[1600px] min-w-[310px] mx-auto max-h-[108px] my-5 md:flex flex-col md:px-12 md:py-6 md:gap-6 lg:px-16 lg:gap-8">
-  <div data-cy="AdminHeader-Logo-Text" className="flex justify-between w-full mb-5">
-    <div className="flex items-center justify-between md:justify-start gap-3">
-      <div className="w-8 h-8 bg-sky-400 rounded-full"></div> 
-      <h1 className="text-4xl">TICKET BOOKING</h1>
+    <div className="px-4 py-4 text-black bg-white max-w-[1600px] min-w-[310px] mx-auto max-h-[108px] my-10 md:flex flex-col md:px-12 md:py-6 md:gap-6 lg:px-16 lg:gap-8">
+      <div data-cy="AdminHeader-Logo-Text" className="flex justify-between w-full mb-5">
+        <div className="flex items-center justify-between md:justify-start gap-3">
+          <div className="w-8 h-8 bg-sky-400 rounded-full"></div>
+          <h1 className="text-4xl">TICKET BOOKING</h1>
+        </div>
+        <div>
+          <Menubar data-cy="AdminHeader-MenuBar-Button">
+            <MenubarMenu>
+              <MenubarTrigger>
+                <User />
+              </MenubarTrigger>
+              <MenubarContent data-cy="AdminHeader-MenuBar-Content-Button-One">
+                <MenubarItem data-cy="">
+                  <div>
+                    Profile
+                    {user?.email}
+                  </div>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem data-cy="AdminHeader-MenuBar-Content-Button-Two">
+                  <div onClick={exitAccount}>
+                    Exit
+                  </div>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+      </div>
+
+      <div data-cy="AdminHeader-Navigation-link" className="text-white flex gap-10">
+        {navigation.map((nav) => (
+          <Link key={nav.link} href={nav.link} className={`relative ${activeLink === nav.link ? 'font-bold text-black' : 'text-gray-800'}`} onClick={() => handleLinkClick(nav.link)}>
+            {nav.name}
+            {activeLink === nav.link && <div className="absolute bottom-0 left-0 w-full h-[2px] bg-orange" />}
+          </Link>
+        ))}
+      </div>
+
+      <div className="border-b-2 border-slate-200"></div>
     </div>
-    <div data-cy="Admin-Header-Exit-Account">
-      <Button onClick={exitAccount}>Гарах</Button> 
-    </div>
-  </div>
-
-  <div data-cy="AdminHeader-Navigation-link" className="text-white flex gap-10">
-    {navigation.map((nav) => (
-      <Link 
-        key={nav.link} 
-        href={nav.link} 
-        className={`relative ${activeLink === nav.link ? 'font-bold text-black' : 'text-gray-800'}`} 
-        onClick={() => handleLinkClick(nav.link)}
-      >
-        {nav.name}
-        {activeLink === nav.link && <div className="absolute bottom-0 left-0 w-full h-[2px] bg-orange" />}
-      </Link>
-    ))}
-  </div>
-
-  <div className="border-b-2 border-slate-200"></div>
-</div>
-
   );
 };
