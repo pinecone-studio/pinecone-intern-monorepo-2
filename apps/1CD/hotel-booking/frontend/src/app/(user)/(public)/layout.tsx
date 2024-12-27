0c7f4b9e896c3f96110a4dfd2cce77ec45d99e44
Nomt <nomttumurkhuu@yahoo.com>
2024-12-27 16:10:36 +0800

    test fix

--- apps/1CD/hotel-booking/frontend/src/app/(user)/(public)/layout.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(user)/(public)/layout.tsx
@@ -2,18 +2,13 @@ import { PropsWithChildren } from 'react';
 import '../.././global.css';
 import FooterHome from '@/components/FooterHome';
 import Header from '@/components/providers/Header';
-import { AuthProvider, SignupProvider } from '@/components/providers';
 
 const PublicLayout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <AuthProvider>
-        <SignupProvider>
-          <Header />
-          {children}
-          <FooterHome />
-        </SignupProvider>
-      </AuthProvider>
+      <Header />
+      {children}
+      <FooterHome />
     </>
   );
 };
