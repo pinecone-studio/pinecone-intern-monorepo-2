011a12caed8d4928d3641c60153ac451417f9771
satsuralala <satsuraltumurbat@gmail.com>
2024-12-29 21:21:06 +0800

    feat: tinder fixed scpecs of swiping

--- apps/1CD/tinder/frontend/specs/components/swipe/swipePage.spec.tsx
+++
@@ -1,8 +0,0 @@
-import Swiper from "@/app/(main)/swipe/functionalities/Swiper"
-import { render } from '@testing-library/react';
-
-describe('should render swipe page',()=>{
-    it('should render',()=>{
-        render(<Swiper/>);
-    });
-});
