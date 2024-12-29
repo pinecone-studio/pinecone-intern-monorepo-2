faca475bfc6dc815f6b1aae5716323cd9a4341b7
Anarchuluu Bayarsaikhan <anarchulu@gmail.com>
2024-12-29 22:08:32 +0800

    720 tinderchat frontend merge (#874)
    * feat(tinder chat schema typedefs adding): tinder chat schema typedefs adding
    
    * feat(tinder chat backend with unit test done): tinder chat backend with unit test done
    
    * feat(tinder backend): tinder chat adding
    
    * feat: tinder chat
    
    * feat(tinder chat): tinder chat frontend and backend
    
    * feat([tinder]): tinder chat
    
    * feat([tinder]): tinder frontend backend
    
    * feat([tinder]): tinder frontend backend
    
    * feat([tinder]): tinder backend and frontend
    
    * feat([tinder]): tinder chat frontend and backend
    
    * feat(frontend): tinde chat frontend
    
    * feat(frontend): removing package.json
    
    * feat(frontend): removing yarn.lock
    
    * feat(tinder): frontend trying to fix bug
    
    * fix(tinder frontend): bug fixing
    
    ---------
    
    Co-authored-by: Anarchuluu Bayarsaikhan <anarchulu@Anarchuluus-MacBook-Air.local>

---
+++ apps/1CD/tinder/frontend/src/components/Chatpart.tsx
@@ -0,0 +1,69 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Oneuser } from './Oneuser';
+import { Loader } from './Loader';
+import { Chatmessages } from './Chatmessages';
+import { Send } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import jwt , { JwtPayload } from 'jsonwebtoken';
+
+type Props = {
+  chatloading: boolean;
+  response: any;
+  errormessage: any;
+  handleMessageChange: any;
+  sendMessage: any;
+  message: string;
+  loading: boolean;
+  authToken:string
+};
+
+export const Chatpart = ({ chatloading, response, errormessage, handleMessageChange, sendMessage, message, loading, authToken }: Props) => {
+  const [userid, setUserid] = useState('')
+  const decoded = jwt.decode(authToken) as JwtPayload | null
+  const userId = decoded?._id
+  useEffect (()=>{
+    if (userId){
+      setUserid(userId)
+      return
+    } else{
+      setUserid('675675e84bd85fce3de34006')
+    }
+  },[])
+  
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') 
+      sendMessage();
+    }
+
+  return (
+    <div className="flex flex-col h-full border-t border-b border-r w-full" data-cy="Chat-Part-Big-Page" >
+      {chatloading ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-col flex-1 h-full">
+          <Oneuser />
+          <div className="flex flex-col flex-1 h-full" >
+
+            <Chatmessages errormessage={errormessage} response={response} user1={userid} />
+            
+            <div className="py-5 px-6 flex gap-4 border-t">
+              <Input placeholder="Say something nice" value={message} onChange={handleMessageChange} data-cy="Chat-Part-Message-Input" onKeyDown={handleKeyDown}  />
+              <Button variant="destructive" className="rounded-full" onClick={sendMessage} data-cy="Chat-Part-Send-Button" disabled={loading}>
+                {loading ? (
+                  'Loading'
+                ) : (
+                  <div className='flex gap-2 items-center'>
+                    <Send size={13}/> Send
+                  </div>
+                )}
+              </Button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
