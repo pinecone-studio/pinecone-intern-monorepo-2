8d9d7d1b4329b85100c85e437aad67feb8ee4007
satsuralala <satsuraltumurbat@gmail.com>
2024-12-29 23:37:03 +0800

    feat: tinder: fixed error in sign uo

--- apps/1CD/tinder/backend/src/resolvers/mutations/user/register-with-email.ts
+++ apps/1CD/tinder/backend/src/resolvers/mutations/user/register-with-email.ts
@@ -11,7 +11,7 @@ export const registerEmail: MutationResolvers['registerEmail'] = async (_, { inp
     await checkExistingEmail(email);
     const otp = generateOTP(email);
     await sendOtpMail(email, otp);
-    await userModel.create({ ...input, otp });
+    await userModel.create({ ...input,interests:'tinder',photos:'tinder',schoolWork:'tinder', password:'tinder1234',age:'21',bio:'tinder',name:'tinder',profession:'tinder',attraction:'tinder',gender:'male',otp });
     return { email };
 
 };
