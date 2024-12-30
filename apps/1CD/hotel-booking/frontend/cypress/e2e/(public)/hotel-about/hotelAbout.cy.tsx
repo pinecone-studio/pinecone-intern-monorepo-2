565552c493b9f7ae2e654649f5453672f20ac48e
24LP4508 24LP4508 <24LP4508@administrators-iMac.local>
2024-12-30 10:38:52 +0800

    feat(aldaa zassan): aldaa zassan

--- apps/1CD/hotel-booking/frontend/cypress/e2e/(public)/hotel-about/hotelAbout.cy.tsx
+++ apps/1CD/hotel-booking/frontend/cypress/e2e/(public)/hotel-about/hotelAbout.cy.tsx
@@ -1,6 +1,6 @@
 describe('HotelDetail', () => {
     beforeEach(() => {
-      cy.visit('/hotel-detail');
+      cy.visit('/hotel-detail/674bfbd6a111c70660b55541');
     });
     it('1. should render', () => {
         cy.scrollTo('bottom').should('exist', '[data-cy="hotel-about"]');
