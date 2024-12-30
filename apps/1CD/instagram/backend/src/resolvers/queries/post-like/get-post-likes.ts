0f4d4f77011a1d362b7ed0c99e505921236058f6
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2024-12-30 13:55:30 +0800

    get postlikes populate add

--- apps/1CD/instagram/backend/src/resolvers/queries/post-like/get-post-likes.ts
+++ apps/1CD/instagram/backend/src/resolvers/queries/post-like/get-post-likes.ts
@@ -3,6 +3,6 @@ import { QueryResolvers } from '../../../generated';
 
 export const getPostLikes: QueryResolvers['getPostLikes'] = async (_, { postId }, { userId }) => {
   if (!userId) throw new Error('Unauthorized');
-  const postLikes = await PostLikeModel.find({ post: postId });
-  return postLikes;
+  const postLikes = await PostLikeModel.find({ post: postId }).populate('user');
+  return postLikes as [];
 };
