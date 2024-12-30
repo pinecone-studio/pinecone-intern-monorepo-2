0e348293f4f5351b4458b74c515c1984ad61d263
Abylai12 <abylai@Aigyerims-MacBook-Air.local>
2024-12-30 13:40:53 +0800

    feat(concert-backend): update priority high and low

--- apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/update-event-high.ts
+++ apps/1CD/concert-ticket-booking/backend/src/resolvers/mutations/event/update-event-high.ts
@@ -3,6 +3,7 @@ import Event from '../../../models/event.model';
 
 export const updateEventHigh: MutationResolvers['updateEventHigh'] = async (_, { eventId }) => {
   const updatedEventPriority = await Event.findByIdAndUpdate({ eventId }, { priority: 'high' }, { new: true });
+
   if (!updatedEventPriority) {
     throw new Error('Event not found');
   }
