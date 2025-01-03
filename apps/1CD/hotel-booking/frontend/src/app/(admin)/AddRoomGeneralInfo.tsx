ae5332655f14dc50b9115e605d3479ce9832ee32
badral <badral@badrals-MacBook-Pro.local>
2025-01-03 18:54:01 +0800

    feat(hotel-booking-frontend): save changes

--- apps/1CD/hotel-booking/frontend/src/app/(admin)/AddRoomGeneralInfo.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(admin)/AddRoomGeneralInfo.tsx
@@ -1,5 +1,4 @@
 import { Input } from '@/components/ui/input';
-
 import { Button } from '@/components/ui/button';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
@@ -52,7 +51,6 @@ const AddRoomGeneralInfo = ({ open, setOpen }: AddHotelGeneralInfoType) => {
         </p>
       );
   };
-
   const RoomType = () => {
     if (formik.errors.roomType && formik.touched.roomType)
       return (
@@ -61,7 +59,6 @@ const AddRoomGeneralInfo = ({ open, setOpen }: AddHotelGeneralInfoType) => {
         </p>
       );
   };
-
   const RoomName = () => {
     if (formik.errors.roomName && formik.touched.roomName)
       return (
