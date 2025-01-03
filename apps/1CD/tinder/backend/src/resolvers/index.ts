61d6fd90728f4b4373c874b65d0f9c02a4970ca3
satsuralala <satsuraltumurbat@gmail.com>
2025-01-03 18:44:26 +0800

    feat(tinder): done the auth

--- apps/1CD/tinder/backend/src/resolvers/index.ts
+++ apps/1CD/tinder/backend/src/resolvers/index.ts
@@ -1,6 +1,8 @@
+
 import { authorization } from '../utils/auth';
 import * as Mutation from './mutations';
 import * as Query from './queries';
+
 
 export const resolvers = {
   Mutation:{
@@ -9,6 +11,19 @@ export const resolvers = {
     birthdaySubmit:authorization(Mutation.birthdaySubmit),
     updateAttraction:authorization(Mutation.updateAttraction),
     swipeUser:authorization(Mutation.swipeUser),
+    createChat:authorization(Mutation.createChat),
+    updateMatch:authorization(Mutation.updateMatch),
+    imageSubmit:authorization(Mutation.imageSubmit),
+    resendOtp:authorization(Mutation.resendOtp),
+    updateUser:authorization(Mutation.updateUser),
+    verifyOtp:authorization(Mutation.verifyOtp),
   },
-  Query,
+  Query:{
+    getUsers:authorization(Query.getUsers),
+    getMatchedUser:authorization(Query.getMatchedUser),
+    getChat:authorization(Query.getChat),
+    getMatch:authorization(Query.getMatch),
+    getOneUser:authorization(Query.getOneUser),
+    
+  }
 };
