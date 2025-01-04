9afb04fef4243421e7d1e72aca8f55d7cf750d66
erkaerka21 <erdenesuren.bat2024@gmail.com>
2025-01-05 00:55:02 +0800

    feat(fe notification dutuu): urgeljluulne

--- apps/1CD/instagram/frontend/src/components/notification/index.tsx
+++ apps/1CD/instagram/frontend/src/components/notification/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import NoNotification from './NoNotification';
 import NotifyFollowRequestCard from '@/app/(main)/_components/NotifyFollowRequestCard';
 import NotifyPostLikeCard from '@/app/(main)/_components/NotifyPostLikeCard';
 import { useGetNotificationsByLoggedUserQuery } from '@/generated';
 
 const Notification = () => {
-  const { data: notifyData } = useGetNotificationsByLoggedUserQuery(context:{Headers:});
+  const token = localStorage.getItem('token');
+  const { data: notifyData } = useGetNotificationsByLoggedUserQuery({ context: { headers: { Authorization: `Bearer ${token}` } } });
   console.log('notify harah', notifyData?.getNotificationsByLoggedUser);
+  useEffect(() => {
+    console.log('notify harah', notifyData);
+  }, [notifyData]);
   return (
     <div className="px-4 py-8 border w-[470px] h-full">
       <h3 className="text-[#262626] text-2xl font-[550] leading-8 tracking-wide mb-5">Notifications</h3>
