c379db1b6698f884e1d51533b2bf4d81e3560032
Abylai12 <abilay1208@gmail.com>
2025-01-05 00:41:11 +0800

    feat(concert-frontend): cancel ticket page done (#1111)
    Co-authored-by: Abylai12 <abylai@Aigyerims-MacBook-Air.local>

--- apps/1CD/concert-ticket-booking/frontend/cypress/utils/orders-mock.ts
+++ apps/1CD/concert-ticket-booking/frontend/cypress/utils/orders-mock.ts
@@ -20,7 +20,7 @@ export const data = {
     {
       _id: '676e59b057bd55115f8bb02c',
       createdAt: '2025-12-27T07:39:28.335Z',
-      status: 'available',
+      status: 'approved',
       ticketType: [
         {
           additional: 'Exclusive merchandise included',
@@ -60,6 +60,14 @@ export const data = {
           soldQuantity: '4',
           unitPrice: '140000',
           zoneName: 'Backstage',
+          __typename: 'TicketType',
+        },
+        {
+          additional: '',
+          discount: '11',
+          soldQuantity: '4',
+          unitPrice: '140000',
+          zoneName: 'Regular',
           __typename: 'TicketType',
         },
       ],
