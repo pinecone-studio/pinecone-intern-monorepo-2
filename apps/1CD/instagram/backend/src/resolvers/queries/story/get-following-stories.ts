79b381b85259f60fe526cdc25a4d88e1562cb314
Myeruyert <myeruyertjanibyek@gmail.com>
2025-01-06 00:23:35 +0800

    1110 story backend edit schema (#1129)
    * added 24h filter, created view-story model, schema
    
    * viewstory backend

--- apps/1CD/instagram/backend/src/resolvers/queries/story/get-following-stories.ts
+++ apps/1CD/instagram/backend/src/resolvers/queries/story/get-following-stories.ts
@@ -4,21 +4,19 @@ import { storyModel, StoryPopulatedType } from 'src/models';
 
 export const getFollowingStories: QueryResolvers['getFollowingStories'] = async (_, __, { userId }) => {
   if (!userId) throw new Error('Unauthorized');
+
   const followings = await followModel.find({ followerId: userId });
 
-  const approvedFollowings = followings.filter((following) => {
-    return following.status === FollowStatus.Approved;
-  });
+  const approvedFollowings = followings.filter((following) => following.status === FollowStatus.Approved);
+
+  const twentyFourHoursAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
 
   const stories = await storyModel
     .find({
-      userId: approvedFollowings.map((item) => {
-        return item.followingId;
-      }),
+      userId: { $in: approvedFollowings.map((item) => item.followingId) },
+      createdAt: { $gte: twentyFourHoursAgo },
     })
     .populate<StoryPopulatedType>('userId');
 
-  return stories.sort((a, b) => {
-    return b.createdAt.valueOf() - a.createdAt.valueOf();
-  });
+  return stories.sort((a, b) => b.createdAt.valueOf() - a.createdAt.valueOf());
 };
