37b88985bf568f2bc0162040c5807dee6badd86d
Myeruyert <myeruyertjanibyek@gmail.com>
2025-01-06 23:16:19 +0800

    edited story backend (#1164)
    Co-authored-by: Myeruyert Janibyek <24LP4972@admins-iMac.local>

--- apps/1CD/instagram/backend/src/models/story.model.ts
+++ apps/1CD/instagram/backend/src/models/story.model.ts
@@ -1,13 +1,10 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose, { Schema, model, models } from 'mongoose';
 import { UserType } from './user.model';
 
 export type StoryType = {
   _id: string;
   userId: Schema.Types.ObjectId;
-  description: string;
-  image: string;
-  createdAt: Date;
-  endDate: string;
+  userStories: [{ story: { _id: Schema.Types.ObjectId; description: string; image: string; createdAt: Date; endDate: string } }];
 };
 
 const storySchema = new Schema<StoryType>(
@@ -17,22 +14,32 @@ const storySchema = new Schema<StoryType>(
       ref: 'userModel',
       required: true,
     },
-    description: {
-      type: String,
-    },
-    image: {
-      type: String,
-    },
-    createdAt: {
-      type: Date,
-      default: new Date(),
-    },
-    endDate: {
-      type: String,
-      default: () => {
-        return new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
+    userStories: [
+      {
+        story: {
+          _id: {
+            type: Schema.Types.ObjectId,
+            default: new mongoose.Types.ObjectId(),
+          },
+          description: {
+            type: String,
+          },
+          image: {
+            type: String,
+          },
+          createdAt: {
+            type: Date,
+            default: new Date(),
+          },
+          endDate: {
+            type: String,
+            default: () => {
+              return new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString();
+            },
+          },
+        },
       },
-    },
+    ],
   },
   { timestamps: true }
 );
