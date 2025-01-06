814b760ce3cbcd2623847a496ee8de5177b82da5
bayarmaa0715 <81559980+bayarmaa0715@users.noreply.github.com>
2025-01-06 13:17:08 +0800

    edit mutation delete and update post (#1139)
    Co-authored-by: Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>

--- apps/1CD/instagram/backend/src/resolvers/mutations/post/delete-post.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/post/delete-post.ts
@@ -1,12 +1,16 @@
 import { MutationResolvers } from '../../../generated';
 import { PostModel } from '../../../models/post.model';
 
-export const deletePost: MutationResolvers['deletePost'] = async (_, { _id }) => {
-  const deletePost = await PostModel.findByIdAndDelete(_id);
+export const deletePost: MutationResolvers['deletePost'] = async (_, { _id }, { userId }) => {
+  const findPost = await PostModel.findById({ _id: _id });
 
-  if (!deletePost) {
-    throw new Error('Can not delete post');
+  if (!findPost) throw new Error('Not found post');
+
+  const userID = findPost.user.toString();
+
+  if (userID === userId) {
+    const deletePost = await PostModel.findByIdAndDelete(_id);
+    return deletePost;
   }
-
-  return deletePost;
+  throw new Error('Can not delete post');
 };
