91410f93106469e6537c8bacda309314c0a718e9
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2025-01-07 12:03:05 +0800

    feat(concert-frontend): delete

--- apps/1CD/concert-ticket-booking/frontend/src/app/admin/home/_components/AdminDashboard.tsx
+++ apps/1CD/concert-ticket-booking/frontend/src/app/admin/home/_components/AdminDashboard.tsx
@@ -179,7 +179,7 @@ export const AdminDashboard = ({ searchValue, selectedValues, date }: AdminDashb
                         <UpdateEventPriority eventId={item!._id} index={index} />
                         <p>edit</p>
                         {loadingDelete ? (
-                          <span className="loader"></span>
+                          <Loader2 className="w-4 h-4 animate-spin text-[#00B7F4]" />
                         ) : (
                           <p onClick={() => handleSubmit(item!._id)}>
                             <Trash className="h-4 w-4" />
