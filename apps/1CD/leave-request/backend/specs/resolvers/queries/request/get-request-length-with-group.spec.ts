49d57e7975dbdb0f0b2739f770d162b737ea7a82
Your Nameclear <amarjargal.ts01@gmail.com>
2025-01-08 13:38:56 +0800

    edit second test

--- apps/1CD/leave-request/backend/specs/resolvers/queries/request/get-request-length-with-group.spec.ts
+++ apps/1CD/leave-request/backend/specs/resolvers/queries/request/get-request-length-with-group.spec.ts
@@ -1,30 +1,35 @@
 import { GraphQLResolveInfo } from 'graphql';
 import { groupedByStatusRequestLength } from 'src/resolvers/queries';
+import { RequestModel } from '../../../../src/models/request';
 
 jest.mock('../../../../src/models/request', () => ({
   RequestModel: {
-    aggregate: jest.fn().mockResolvedValueOnce([
-      {
-        label: "pending",
-        res: 1
-      },
-    ]),
+    aggregate: jest.fn() as jest.Mock,  // Mock-г TypeScript-ийг зөв ойлгохыг зааж өгнө
   },
 }));
 
 describe('check supervisor if exist', () => {
   it("supervisor that doesn't exist", async () => {
+    // aggregate-ийг mockResolvedValueOnce ашиглан тохируулах
+    (RequestModel.aggregate as jest.Mock).mockResolvedValueOnce([
+      {
+        label: "pending",
+        res: 1
+      },
+    ]);
+
     const res = await groupedByStatusRequestLength!(
       {},
-      { email: 'zolookorzoloo@gmail.com', startDate: '2024-12-04T07:28:05.099Z', endDate: '2024-12-23T07:28:05.099Z', status: 'pending' },
+      { input: { email: 'zolookorzoloo@gmail.com', startDate: '2024-12-04T07:28:05.099Z', endDate: '2024-12-23T07:28:05.099Z' } },
       {},
       {} as GraphQLResolveInfo
     );
+
     expect(res).toEqual([
-        {
-          label: "pending",
-          res: 1
-        },
-      ]);
+      {
+        label: "pending",
+        res: 1
+      },
+    ]);
   });
 });
