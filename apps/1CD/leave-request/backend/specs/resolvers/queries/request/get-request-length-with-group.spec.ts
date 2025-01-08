33378a47bd251f0fec7ade48d98705648a086881
Your Nameclear <amarjargal.ts01@gmail.com>
2025-01-08 14:44:18 +0800

    change email to supervisorEmail second test

--- apps/1CD/leave-request/backend/specs/resolvers/queries/request/get-request-length-with-group.spec.ts
+++ apps/1CD/leave-request/backend/specs/resolvers/queries/request/get-request-length-with-group.spec.ts
@@ -12,7 +12,7 @@ const mockAggregate = jest.requireMock('../../../../src/models/request').Request
 describe('groupedByStatusRequestLength Resolver', () => {
   const fixedDate = new Date('2023-01-01T00:00:00Z');
   const commonParams = {
-    email: 'supervisor@example.com',
+    supervisorEmail: 'supervisor@example.com',
     startDate: fixedDate,
     endDate: fixedDate,
   };
@@ -22,24 +22,20 @@ describe('groupedByStatusRequestLength Resolver', () => {
   });
 
   it('should return grouped request length based on status', async () => {
-    // Mock response from the aggregate function
     const mockData = [
       { _id: 'sent', res: 3 },
       { _id: 'pending', res: 5 },
     ];
 
-    // Mock the aggregate function to return mock data
     mockAggregate.mockResolvedValue(mockData);
 
-    // Call the resolver function with the mock parameters
     const result = await groupedByStatusRequestLength!({}, { input: { ...commonParams, status: ['sent', 'pending'] } }, {}, {} as GraphQLResolveInfo);
 
-    // Assert that the result matches the mock data
     expect(result).toEqual(mockData);
     expect(mockAggregate).toHaveBeenCalledWith([
       { 
         $match: { 
-          supervisorEmail: commonParams.email, 
+          supervisorEmail: commonParams.supervisorEmail, 
           requestDate: { 
             $gte: expect.any(Date), 
             $lte: expect.any(Date),
@@ -55,25 +51,20 @@ describe('groupedByStatusRequestLength Resolver', () => {
       { 
         $sort: { count: -1 },
       },
-    ]); // Check if the aggregation pipeline was called correctly
+    ]); 
   });
 
   it('should return empty array if no requests match', async () => {
-    // Mock an empty result from aggregate
     mockAggregate.mockResolvedValue([]);
 
-    // Call the resolver with parameters
     const result = await groupedByStatusRequestLength!({}, { input: { ...commonParams, status: ['sent', 'pending'] } }, {}, {} as GraphQLResolveInfo);
 
-    // Assert that the result is an empty array
     expect(result).toEqual([]);
   });
 
   it('should throw an error on aggregation failure', async () => {
-    // Mock an error response from the aggregate function
     mockAggregate.mockRejectedValue(new Error('Database error'));
 
-    // Assert that an error is thrown
     await expect(
       groupedByStatusRequestLength!({}, { input: { ...commonParams, status: ['sent'] } }, {}, {} as GraphQLResolveInfo)
     ).rejects.toThrow('Database error');
