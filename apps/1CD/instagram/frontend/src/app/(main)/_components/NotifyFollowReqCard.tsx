aa65626cb3bb7b48fef823f700fb1a46d69e53df
erkaerka21 <erdenesuren.bat2024@gmail.com>
2025-01-08 09:56:19 +0800

    feat(fe follow request notify): angid urgeljluuleh

--- apps/1CD/instagram/frontend/src/app/(main)/_components/NotifyFollowReqCard.tsx
+++ apps/1CD/instagram/frontend/src/app/(main)/_components/NotifyFollowReqCard.tsx
@@ -1,20 +1,26 @@
 import { Button } from '@/components/ui/button';
+import { AccountVisibility } from '@/generated';
+import { formatDistanceToNowStrict } from 'date-fns';
 import Image from 'next/image';
-const NotifyFollowRequestCard = () => {
+
+type FollowReqNotifyType = { accountVisible: AccountVisibility; profileImg: string; userName: string; createdDate: Date };
+const NotifyFollowRequestCard = ({ accountVisible, profileImg, userName, createdDate }: FollowReqNotifyType) => {
+  const dateDistance = formatDistanceToNowStrict(createdDate);
+
   return (
     <>
       {accountVisible === 'PRIVATE' && (
         <div className="flex items-center justify-between gap-4 px-3 py-2">
           <div className="flex items-center gap-3">
             <div className="relative flex rounded-full w-[44px] h-[44px]">
-              <Image fill={true} src="/images/img.avif" alt="Photo1" className="h-full rounded-full w-fit" />
+              <Image fill={true} src={profileImg} alt="Photo1" className="absolute object-cover h-full rounded-full" />
             </div>
-            <div className="flex flex-col text-[#09090B] max-w-28">
-              <span className="text-sm ">username</span>
-              <div className="text-xs">
-                <span className="mr-1">has requested to follow you</span>
-                <span className="text-[#71717A]">10m</span>
+            <div className="flex flex-col text-[#09090B]">
+              <div className="">
+                <span className="text-sm font-semibold">{userName}</span>
+                <span className="ml-2 text-sm">has requested to follow you</span>
               </div>
+              <span className="text-[#71717A] text-xs">{dateDistance}</span>
             </div>
           </div>
           <div className="flex gap-2">
@@ -31,22 +37,19 @@ const NotifyFollowRequestCard = () => {
         <div className="flex items-center justify-between gap-4 px-3 py-2">
           <div className="flex items-center gap-3">
             <div className="relative flex rounded-full w-[44px] h-[44px]">
-              <Image fill={true} src="/images/img.avif" alt="Photo1" className="h-full rounded-full w-fit" />
+              <Image fill={true} src={profileImg} alt="Photo1" className="absolute object-cover h-full rounded-full" />
             </div>
-            <div className="flex flex-col text-[#09090B] max-w-28">
-              <span className="text-sm ">username</span>
-              <div className="text-xs">
-                <span className="mr-1">started following you</span>
-                <span className="text-[#71717A]">10m</span>
+            <div className="flex flex-col text-[#09090B]">
+              <div className="">
+                <span className="text-sm font-semibold">{userName}</span>
+                <span className="ml-2 text-sm">started following you</span>
               </div>
+              <span className="text-[#71717A] text-xs">{dateDistance}</span>
             </div>
           </div>
           <div className="flex gap-2">
             <Button className="bg-[#2563EB] rounded-lg text-[#FAFAFA]" data-testid="confirm-btn">
-              Confirm
-            </Button>
-            <Button className="bg-[#F4F4F5] rounded-lg text-[#18181B]" data-testid="delete-btn">
-              Delete
+              Follow
             </Button>
           </div>
         </div>
--- apps/1CD/instagram/frontend/src/components/notification/index.tsx
+++ apps/1CD/instagram/frontend/src/components/notification/index.tsx
@@ -3,20 +3,43 @@ import NotifyFollowRequestCard from '@/app/(main)/_components/NotifyFollowReqCar
 import NotifyPostLikeCard from '@/app/(main)/_components/NotifyPostLikeCard';
 import NoNotification from '../../app/(main)/_components/NoNotification';
 import { useGetNotificationsByLoggedUserQuery } from '@/generated';
+import { useAuth } from '../providers';
 
 const Notification = () => {
+  const { user } = useAuth();
+  const accountVis = user?.accountVisibility;
   const { data: notifyData, loading } = useGetNotificationsByLoggedUserQuery();
   if (loading) return <p data-testid="notificationLoading">loading...</p>;
+  console.log('datanuudiig harah', notifyData);
   return (
     <div className="px-4 py-8 border w-[470px] h-full" data-testid="notification-component">
       <h3 className="text-[#262626] text-2xl font-[550] leading-8 tracking-wide mb-5">Notifications</h3>
       <div className="flex flex-col gap-4">
         <h6>Today</h6>
       </div>
-      <NotifyFollowRequestCard />
-      {notifyData?.getNotificationsByLoggedUser!.map((oneNotification) => (
-        <NotifyPostLikeCard key={oneNotification._id} userName={oneNotification.otherUserId.userName!} profileImg={oneNotification.otherUserId.profileImg!} createdDate={oneNotification.createdAt} />
-      ))}
+
+      {notifyData?.getNotificationsByLoggedUser!.map((oneNotification) => {
+        if (oneNotification.notificationType === 'POSTLIKE') {
+          return (
+            <NotifyPostLikeCard
+              key={oneNotification._id}
+              userName={oneNotification.otherUserId.userName!}
+              profileImg={oneNotification.otherUserId.profileImg!}
+              createdDate={oneNotification.createdAt}
+            />
+          );
+        } else if (oneNotification.notificationType === 'FOLLOW') {
+          return (
+            <NotifyFollowRequestCard
+              key={oneNotification._id}
+              userName={oneNotification.otherUserId.userName}
+              profileImg={oneNotification.otherUserId.profileImg!}
+              createdDate={oneNotification.createdAt}
+              accountVisible={accountVis!}
+            />
+          );
+        }
+      })}
       <NoNotification />
     </div>
   );
