5c636bbda51f2a87df4186339cf776a370330bc2
satsuralala <satsuraltumurbat@gmail.com>
2025-01-08 12:51:45 +0800

    feat: tidner get me done (#1235)

--- apps/1CD/tinder/backend/src/utils/token.ts
+++ apps/1CD/tinder/backend/src/utils/token.ts
@@ -1,18 +1,9 @@
-import jwt from 'jsonwebtoken';
-import { NextRequest } from 'next/server';
-export const getUserId = (req: NextRequest) => {
-  const secretKey = `${process.env.TOKEN_SECRET}`;
-  const authHeader = `${req.headers.get('Authorization')}`;
-  const authToken = authHeader?.split(' ')[1];
-  const isDev = process.env.NODE_ENV === 'development';
-  
 
-  if (!isDev && authToken) {
-    const { userId } = <jwt.JwtPayload>jwt.verify(authToken, secretKey);
-    if (!userId) {
-      return null;
-    }
-    return userId;
-  }
-  return '675675e84bd85fce3de34006';
+import { NextRequest } from 'next/server';
+import { giveTokenIndev } from './for-dev-token';
+import { checkTokenInProd } from './for-prod-token';
+export const getUserId = (req: NextRequest) => {
+  const tokenForDev=giveTokenIndev();
+  const tokenForProd=checkTokenInProd({ req }); 
+  return tokenForDev || tokenForProd;
 };
