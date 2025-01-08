78ddaf46d235cccb0d2276806ee786e140f94448
badral <badral@badrals-MacBook-Pro.local>
2025-01-08 13:13:53 +0800

    feat(hotel-booking-frontend): cypress changed

--- apps/1CD/hotel-booking/frontend/src/components/providers/CheckLoginUser.tsx
+++ apps/1CD/hotel-booking/frontend/src/components/providers/CheckLoginUser.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { useAuth } from './AuthProvider';
 import { ReactNode } from 'react';
 
 const CheckLoginUser = ({ children }: { children: ReactNode }) => {
   const { user } = useAuth();
-  const router = useRouter();
+  // const router = useRouter();
   if (!user) {
     // router.push('/login');
     return null;
