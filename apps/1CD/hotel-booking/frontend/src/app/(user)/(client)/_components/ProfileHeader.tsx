0978beffa7fe58c0aaa09af5844a5983bbfba10a
Nomt <nomttumurkhuu@yahoo.com>
2025-01-10 02:30:25 +0800

    feat(hotel-booking-frontend): profile page frontend and test

---
+++ apps/1CD/hotel-booking/frontend/src/app/(user)/(client)/_components/ProfileHeader.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+
+const ProfileHeader = () => {
+  const data = {
+    firstName: 'Shagai',
+    email: 'shagai@pinecone.mn',
+  };
+  return (
+    <div className="w-2/3 px-10 py-4 border-b-2 border-gray-200" data-cy="Update-Profile-Header">
+      <div className="w-full">
+        <h1 className="text-3xl font-semibold">Hi, {data.firstName}</h1>
+        <p className="text-xs text-gray-400">{data.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileHeader;
