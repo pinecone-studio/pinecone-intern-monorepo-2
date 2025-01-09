86b4cd3ad686224f1afe335bdbe6b42e4d79f19c
24LP2640 24LP2640 <24LP2640@administrators-iMac.local>
2025-01-09 17:29:43 +0800

    supervisor format date

--- apps/1CD/leave-request/frontend/src/components/myreq/SentRequest.tsx
+++ apps/1CD/leave-request/frontend/src/components/myreq/SentRequest.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { ClientDatePicker } from './DatePicker';
 import { useGetRequestsQuery } from '@/generated';
 import { Calendar, Tag } from 'lucide-react';
-import { addDays, format } from 'date-fns';
+import { addDays, format, formatDate } from 'date-fns';
 import { useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { Loader } from '@/context/SecurePageWrapper';
@@ -26,8 +26,8 @@ const RequestGroup = ({ date, requests }: { date?: string; requests: Array<reque
   if (!requests || !date) {
     return null;
   }
-
-  const formatedDate = date?.split('-').slice(1).join('-');
+console.log(date)
+  const formatedDate = formatDate(new Date(date), 'MM-dd');
   const dateObj = new Date(date).setHours(0, 0, 0, 0);
   const today = new Date().setHours(0, 0, 0, 0);
   const gap = Math.abs(dateObj - today) / (1000 * 60 * 60 * 24);
