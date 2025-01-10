1cbcea23c2c0ebae78acb9ee258cd896f0cebcca
Erdenesuren Batbaatar <erdenesuren.bat2024@gmail.com>
2025-01-10 13:06:15 +0800

    1313 ig fe notification sortloh bolon viewed iin bg tsagaan boloh (#1327)
    * feat(fe sort done, odoo filter bolon): viewed ni bg tsagaantai boloh
    
    * notification

--- apps/1CD/instagram/frontend/src/app/(main)/_components/NotifyPostLikeCard.tsx
+++ apps/1CD/instagram/frontend/src/app/(main)/_components/NotifyPostLikeCard.tsx
@@ -1,10 +1,10 @@
 import { Heart } from 'lucide-react';
 import Image from 'next/image';
 import { formatDistanceToNowStrict } from 'date-fns';
-const NotifyPostLikeCard = ({ userName, profileImg, createdDate }: { userName: string; profileImg: string; createdDate: Date }) => {
+const NotifyPostLikeCard = ({ userName, profileImg, createdDate, isViewed, onClick }: { userName: string; profileImg: string; createdDate: Date; isViewed: boolean; onClick: () => Promise<void> }) => {
   const dateDistance = formatDistanceToNowStrict(createdDate);
   return (
-    <div className="flex items-center justify-between gap-4 px-3 py-2 bg-sky-50" data-cy="notify-postlike-card">
+    <div className={`flex items-center justify-between gap-4 px-3 py-2 ${isViewed ? '' : 'bg-sky-50'}`} data-cy="notify-postlike-card" onClick={onClick}>
       <div className="flex items-center gap-3">
         <section className="relative flex rounded-full w-[44px] h-[44px]">
           <Image fill={true} src={profileImg} alt="profile-image" className="absolute object-cover h-full rounded-full" data-cy="notify-postlike-proImg" />
