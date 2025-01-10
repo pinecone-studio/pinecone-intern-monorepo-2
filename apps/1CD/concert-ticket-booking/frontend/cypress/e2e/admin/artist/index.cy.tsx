4bc7fd015b6ae4de0460f03d6919aa8445ffb247
Tommy9901 <temuuujin0317@gmail.com>
2025-01-10 19:52:07 +0800

    feat: 22

--- apps/1CD/concert-ticket-booking/frontend/cypress/e2e/admin/artist/index.cy.tsx
+++ apps/1CD/concert-ticket-booking/frontend/cypress/e2e/admin/artist/index.cy.tsx
@@ -29,9 +29,31 @@
 //     cy.get('[data-cy=loading-text]').should('be.visible');
 //   });
 
-//   it('should render artis page', () => {
-//     cy.get('[data-cy="table-header"]').should('be.visible');
-//     cy.get('[data-cy="Artist-Components"]').find('h3').should('contain.text', 'Артист');
-//     cy.get('[data-cy="Artist-Components"]').find('p').should('contain.text', 'Бүх артистийн мэдээлэл');
-//   });
-// });
+describe('Artist Page', () => {
+    beforeEach(() => {
+        cy.visit('/user/sign-in');
+        cy.get('input[name="email"]').type('adminDev@gmail.com');
+        cy.get('input[name="password"]').type('@Pi88923');
+        cy.get('[data-cy="Sign-In-Submit-Button"]').should('be.visible').click();
+        cy.get('.toast').should('contain', 'Successfully login');
+        cy.url().should('include', '/admin/home');
+        cy.visit('/admin/artist');
+      });
+  
+    it('should render the page with correct header and information', () => {
+      cy.get('[data-cy="Artist-Components"]').should('exist');
+      // Check for the artist page title and description
+      cy.get('h3').should('have.text', 'Артист');
+      cy.get('p').should('have.text', 'Бүх артистийн мэдээлэл');
+  
+    });
+
+    it('should render the copyright text', () => {
+        cy.get('[data-cy="Admin-Footer"]').should('be.visible');
+        cy.get('[data-cy="Admin-Footer"]').should('contain.text', '©2024 Copyright');
+      });
+  
+   
+  });
+
+  
