9af437bffa8a2aa405d0107b27711b4fd31360f8
Nomt <nomttumurkhuu@yahoo.com>
2025-01-10 17:10:10 +0800

    feat(hotel-booking-backend): getUser test

--- apps/1CD/hotel-booking/backend/src/resolvers/queries/users/get-user.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/users/get-user.ts
@@ -2,8 +2,7 @@ import { QueryResolvers } from 'src/generated';
 import { userModel } from 'src/models';
 
 export const getUser: QueryResolvers['getUser'] = async (_, __, { userId }) => {
-  console.log('user', userId);
-  const user = await userModel.findOne({ _id: userId });
-  if (!user) throw new Error('email is not found');
+  if (!userId) throw new Error('email is not found');
+  const user = await userModel.findById({ _id: userId });
   return user;
 };
