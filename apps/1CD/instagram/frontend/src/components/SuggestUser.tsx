7e84af3d7ab4fbd8d4edbc41399b32fada8e260b
Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>
2025-01-10 17:29:51 +0800

    suggest user in frontend

--- apps/1CD/instagram/frontend/src/components/SuggestUser.tsx
+++ apps/1CD/instagram/frontend/src/components/SuggestUser.tsx
@@ -22,7 +22,7 @@ export const SuggestUser = () => {
     <>
       {SuggestUser?.map((user) => {
         return (
-          <div data-testid="suggest-user-comp" className="flex items-center justify-between w-full">
+          <div key={user?.followerId._id} data-testid="suggest-user-comp" className="flex items-center justify-between w-full">
             <div className="flex items-center gap-2">
               <div className="relative flex w-8 h-8 rounded-full">
                 <Image fill={true} src={user?.followingId.profileImg || '/images/profileImg.webp'} alt="Photo1" className="w-auto h-auto rounded-full" sizes="w-auto h-auto" priority />
