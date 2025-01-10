af27280b092e43d383938758d6982599f557521b
bayarmaa0715 <81559980+bayarmaa0715@users.noreply.github.com>
2025-01-10 23:44:33 +0800

    comment like mutation (#1398)
    Co-authored-by: Bayarmaa Munkhbat <24LP2392@administrators-iMac.local>

--- apps/1CD/instagram/backend/src/models/comment-like.model.ts
+++ apps/1CD/instagram/backend/src/models/comment-like.model.ts
@@ -1,20 +1,36 @@
 import { model, models, Schema } from 'mongoose';
+import { UserType } from './user.model';
 
 export type commentLikeType = {
   _id: string;
-  commentId: Schema.Types.ObjectId;
-  isLiked: boolean;
+  comment: Schema.Types.ObjectId;
+  isLike: boolean;
   likedUser: Schema.Types.ObjectId;
   createdAt: Date;
 };
 
 const commentLikeSchema = new Schema<commentLikeType>({
-  commentId: { type: Schema.Types.ObjectId, required: true, ref: 'commentModel' },
-  isLiked: { type: Boolean, default: false },
-  likedUser: { type: Schema.Types.ObjectId, required: true, ref: 'userModel' },
+  comment: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'commentModel',
+  },
+  isLike: {
+    type: Boolean,
+    default: false,
+  },
+  likedUser: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'userModel',
+  },
   createdAt: {
     type: Date,
     default: new Date(),
   },
 });
-export const commentLikeModel = models['commentLiekModel'] || model('commentLikeModel', commentLikeSchema);
+export type PopulatedType = Omit<commentLikeType, 'likedUser'> & {
+  user: UserType;
+};
+
+export const commentLikeModel = models.commentLikeModel || model<commentLikeType>('commentLikeModel', commentLikeSchema);
