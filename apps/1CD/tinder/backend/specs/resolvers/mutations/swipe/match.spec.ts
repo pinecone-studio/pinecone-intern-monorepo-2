da0dd9aa21817e26be7c9ec58346d7936e81101c
satsuralala <satsuraltumurbat@gmail.com>
2025-01-11 18:22:39 +0800

    1430 tinderfixing swipe 1 (#1439)
    * feat: added swiping
    
    * feat: tinder

--- apps/1CD/tinder/backend/specs/resolvers/mutations/swipe/match.spec.ts
+++ apps/1CD/tinder/backend/specs/resolvers/mutations/swipe/match.spec.ts
@@ -1,12 +1,19 @@
 import { getMatchedUser } from "../../../../src/resolvers/queries";
-import {  userModel } from "../../../../src/models";
+import {  Matchmodel, userModel } from "../../../../src/models";
 import { GraphQLError, GraphQLResolveInfo } from "graphql";
+
 
 jest.mock('../../../../src/models/user/user.model',()=>({
     userModel:{
         findById:jest.fn()
+    },
+    Matchmodel:{
+        find:jest.fn()
     }
 }));
+beforeEach(() => {
+    jest.clearAllMocks();
+  });
 describe('should test matched user query',()=>{
      const mockInfo = {} as GraphQLResolveInfo;
      const mockUser1={
@@ -20,8 +27,7 @@ describe('should test matched user query',()=>{
 
     };
     it('should successfully return data',async()=>{
-       
-
+        (Matchmodel.find as jest.Mock).mockResolvedValueOnce({user1:mockUser1.id, user2:mockUser2.id,matched:'true'});
         (userModel.findById as jest.Mock).mockResolvedValueOnce(mockUser1);
         (userModel.findById as jest.Mock).mockResolvedValueOnce(mockUser2);
         const result = await getMatchedUser!({}, { matchedUser:mockUser1.id }, {userId:mockUser2.id}, mockInfo);
@@ -32,10 +38,24 @@ describe('should test matched user query',()=>{
         })
 
     });
+    it('should successfully return data',async()=>{
+        (Matchmodel.find as jest.Mock).mockResolvedValueOnce({user1:mockUser1.id, user2:mockUser2.id,matched:'true'});
+        (userModel.findById as jest.Mock).mockResolvedValueOnce(mockUser1);
+        (userModel.findById as jest.Mock).mockResolvedValueOnce(mockUser2);
+        const result = await getMatchedUser!({}, { matchedUser:mockUser1.id }, {userId:mockUser2.id}, mockInfo);
+        expect(result).toEqual({
+            swipedUserImg:mockUser1.photos[0],
+            userImg:mockUser2.photos[0],
+            swipedName:mockUser1.name
+        })
+
+    });
+    
     it('should throw new error',async()=>{
-        (userModel.findById as jest.Mock).mockResolvedValue(null);
+        (userModel.findById as jest.Mock).mockResolvedValueOnce(null);
+        (Matchmodel.find as jest.Mock).mockResolvedValueOnce(null);
         expect( getMatchedUser!({}, { matchedUser:mockUser1.id }, {userId:mockUser2.id}, mockInfo)).rejects.toThrow(GraphQLError);
-        expect( getMatchedUser!({}, { matchedUser:mockUser1.id }, {userId:mockUser2.id}, mockInfo)).rejects.toThrow('database error in getUser in matching');
+        expect( getMatchedUser!({}, { matchedUser:mockUser1.id }, {userId:mockUser2.id}, mockInfo)).rejects.toThrow('no matched user found');
         
     })
 })
--- apps/1CD/tinder/backend/specs/resolvers/queries/swipe/get-users.spec.ts
+++ apps/1CD/tinder/backend/specs/resolvers/queries/swipe/get-users.spec.ts
@@ -1,45 +1,100 @@
-import { userModel } from "../../../../src/models";
-import { getUsers } from "../../../../src/resolvers/queries";
-import { GraphQLError, GraphQLResolveInfo } from "graphql";
+import { GraphQLResolveInfo } from 'graphql';
+import { userModel } from '../../../../src/models';
+import { getUsers } from '../../../../src/resolvers/queries';
 
-jest.mock('../../../../src/models',()=>({
-    userModel:{
-        find:jest.fn()
-    }
+
+jest.mock('../../../../src/models', () => ({
+  userModel: {
+    find: jest.fn(),
+    findById: jest.fn(),
+  },
 }));
 
-describe('should return user',()=>{
-    const users=[
-        {
-            _id:'123',
-            name:'user1',
-            email:'cypress@gmail.com',
-            photos:'jjjj',
-        },
-        {
-            _id:'1234',
-            name:'user1',
-            email:'cypress@gmail.com',
-            photos:'jjjj',
-        },
-        {
-            _id:'12345',
-            name:'user1',
-            email:'cypress@gmail.com',
-            photos:'jjjj',
-        }
-    ];
-    beforeEach(()=>{
-        jest.clearAllMocks()
-    })
-    it('should successfully return users',async()=>{
-        (userModel.find as jest.Mock).mockResolvedValue(users);
-        const res=await getUsers!({},{},{userId:'675675e84bd85fce3de34006'},{} as GraphQLResolveInfo);
-        expect(res).toBe(users);
-    });
-    it('should throw error when users could not found',async()=>{
-        (userModel.find as jest.Mock).mockRejectedValue(null);
-        await expect(getUsers!({},{},{userId:'675675e84bd85fce3de34006'},{} as GraphQLResolveInfo)).rejects.toThrow(GraphQLError);
-        await expect(getUsers!({},{},{userId:'675675e84bd85fce3de34006'},{} as GraphQLResolveInfo)).rejects.toThrow('could not find the users');
-    })
-})
+describe('should return user', () => {
+//   const users = [
+//     {
+//       _id: '123',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//       gender: 'both',
+//     },
+//     {
+//       _id: '1234',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//       gender: 'both',
+//     },
+//     {
+//       _id: '12345',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//      gender: 'female',
+//     },
+//   ];
+//   const users1 = [
+//     {
+//       _id: '123',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//       gender: 'male',
+//     },
+//     {
+//       _id: '1234',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//       gender: 'male',
+//     },
+//     {
+//       _id: '12345',
+//       name: 'user1',
+//       email: 'cypress@gmail.com',
+//       photos: 'jjjj',
+//       gender:'male'
+//     },
+//   ];
+//   const user = {
+//     _id: '12345',
+//     name: 'user1',
+//     email: 'cypress@gmail.com',
+//     photos: 'jjjj',
+//     attraction:'both'
+//   };
+//   const user1 = {
+//     _id: '12345',
+//     name: 'user1',
+//     email: 'cypress@gmail.com',
+//     photos: 'jjjj',
+//     attraction:'male'
+//   };
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should successfully return users with attraction both', async () => {
+    (userModel.findById as jest.Mock).mockResolvedValueOnce({ attraction: 'both' });
+    (userModel.find as jest.Mock).mockResolvedValueOnce([{
+        _id: '123',
+        name: 'user1',
+        email: ""
+    }]);
+    const res = await getUsers!({}, {}, { userId: '675675e84bd85fce3de34006' }, {} as GraphQLResolveInfo);
+    expect(res).toEqual([{ _id: '123', name: 'user1', email: "" }]);
+  });
+  it('should return user if user attract to female',async () => {
+    (userModel.findById as jest.Mock).mockResolvedValueOnce({ attraction:""});
+    (userModel.find as jest.Mock).mockResolvedValueOnce([{
+        _id: '123',
+        name: 'user1',
+        email: "",
+        
+    }]);
+    const res = await getUsers!({}, {}, { userId: '675675e84bd85fce3de34006' }, {} as GraphQLResolveInfo);
+    expect(res).toEqual([{ _id: '123', name: 'user1', email: "" }]);
+  });
+
+});
