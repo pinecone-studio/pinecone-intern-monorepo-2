f2c77a3e4622a57576246484c568f6de9bacd4e9
satsuralala <satsuraltumurbat@gmail.com>
2025-01-11 17:52:09 +0800

    feat: added swiping

--- apps/1CD/tinder/backend/src/resolvers/queries/swipe/get-users.ts
+++ apps/1CD/tinder/backend/src/resolvers/queries/swipe/get-users.ts
@@ -1,13 +1,19 @@
 
-import { GraphQLError } from "graphql";
+
 import { userModel } from "../../../models"
 import { QueryResolvers } from "../../../generated";
+import { Context } from "../../../types";
 
-export const getUsers:QueryResolvers['getUsers']=async()=>{
-    try{
-        const users=await userModel.find({});
+export const getUsers:QueryResolvers['getUsers']=async(_,__,{userId}:Context)=>{
+
+        const user=await userModel.findById({_id:userId});
+
+       
+        if(user.attraction==='both'){
+            const users=await userModel.find({ _id: { $ne: userId } });
+            return users;
+        }
+        const users=await userModel.find({ _id: { $ne: userId } , gender:user.attraction});
         return users;
-    }catch(error){
-        throw new GraphQLError('could not find the users')
-    }
+    
 }
