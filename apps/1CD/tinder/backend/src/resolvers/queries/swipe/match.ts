272dcdb19cc48299f63fd16ed8559b0af32939c0
satsuralala <satsuraltumurbat@gmail.com>
2025-01-11 18:16:11 +0800

    feat: tinder

--- apps/1CD/tinder/backend/src/resolvers/queries/swipe/match.ts
+++ apps/1CD/tinder/backend/src/resolvers/queries/swipe/match.ts
@@ -1,18 +1,14 @@
 import { GraphQLError } from "graphql";
 import { QueryResolvers } from "../../../generated";
-import { userModel } from "../../../models";
+import { Matchmodel, userModel } from "../../../models";
 import { Context } from "../../../types";
 
 export const getMatchedUser:QueryResolvers['getMatchedUser']=async(_,{matchedUser},{userId}:Context,)=>{
-    try{
-        const swipedOne=await userModel.findById(matchedUser);
-        const swipingOne=await userModel.findById(userId);
-        console.log(swipedOne.name)
-
-        return {swipedUserImg:swipedOne.photos[0], userImg:swipingOne.photos[0],swipedName:swipedOne.name}
-    }catch(error){
-        console.log(error)
-        throw new GraphQLError('database error in getUser in matching')
-    }
-
+        const matchedUsers=await Matchmodel.find({user1:userId, user2:matchedUser,matched:'true'})
+        if(matchedUsers){
+            const swipedOne=await userModel.findById(matchedUser);
+            const swipingOne=await userModel.findById(userId);
+            return {swipedUserImg:swipedOne.photos[0], userImg:swipingOne.photos[0],swipedName:swipedOne.name}
+        }
+        throw new GraphQLError('no matched user found')
 }
