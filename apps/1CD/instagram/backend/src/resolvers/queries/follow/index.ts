d628ab37fa437fe23ee29466f5064e9bb3bdde79
Erdenesuren Batbaatar <erdenesuren.bat2024@gmail.com>
2025-01-11 16:04:33 +0800

    IG bE query index

--- apps/1CD/instagram/backend/src/resolvers/queries/follow/index.ts
+++ apps/1CD/instagram/backend/src/resolvers/queries/follow/index.ts
@@ -2,3 +2,4 @@ export * from './see-followers-query';
 export * from './see-followings-query';
 export * from './get-follow-status-query';
 export * from './get-suggest-user';
+export * from './get-follow-status-by-following-id-query';
