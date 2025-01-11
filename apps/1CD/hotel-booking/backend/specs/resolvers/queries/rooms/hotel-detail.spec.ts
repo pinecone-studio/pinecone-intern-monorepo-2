d2cbaa07ea8c8fbd75e69210a0f281926e5eed60
badral <badralbaigalmaa7@gmail.com>
2025-01-11 17:22:01 +0800

    feat: hotel-booking-backend room filter (#1432)
    Co-authored-by: badral <badral@badrals-MacBook-Pro.local>

--- apps/1CD/hotel-booking/backend/specs/resolvers/queries/rooms/hotel-detail.spec.ts
+++ apps/1CD/hotel-booking/backend/specs/resolvers/queries/rooms/hotel-detail.spec.ts
@@ -5,24 +5,50 @@ jest.mock('src/models', () => ({
   roomsModel: {
     find: jest
       .fn()
-      .mockReturnValueOnce({
-        _id: '1',
-        roomName: 'test',
-      })
-      .mockReturnValueOnce(null),
+      .mockReturnValueOnce([
+        {
+          _id: '1',
+          roomName: 'test',
+        },
+      ])
+      .mockReturnValueOnce([]),
+  },
+  bookingModel: {
+    find: jest
+      .fn()
+      .mockReturnValueOnce([])
+      .mockReturnValueOnce([{ roomId: '1' }]),
   },
 }));
 describe('get-rooms', () => {
+  const input = {
+    roomType: 'test',
+    checkInDate: '2024-12-10',
+    checkOutDate: '2025-12-15',
+  };
   it('should return room', async () => {
-    const response = await hotelDetail!({}, { hotelId: '1' }, { userId: '1' }, {} as GraphQLResolveInfo);
-    expect(response).toEqual({
-      _id: '1',
-      roomName: 'test',
-    });
+    const response = await hotelDetail!(
+      {},
+      {
+        hotelId: '1',
+        input: {
+          ...input,
+          roomType: '',
+        },
+      },
+      { userId: '1' },
+      {} as GraphQLResolveInfo
+    );
+    expect(response).toEqual([
+      {
+        _id: '1',
+        roomName: 'test',
+      },
+    ]);
   });
   it('should return null', async () => {
     try {
-      await hotelDetail!({}, { hotelId: '1' }, { userId: '1' }, {} as GraphQLResolveInfo);
+      await hotelDetail!({}, { hotelId: '1', input }, { userId: '1' }, {} as GraphQLResolveInfo);
     } catch (err) {
       expect((err as Error).message).toEqual('Rooms not found');
     }
--- apps/1CD/hotel-booking/backend/src/resolvers/queries/rooms/hotel-detail.ts
+++ apps/1CD/hotel-booking/backend/src/resolvers/queries/rooms/hotel-detail.ts
@@ -1,13 +1,40 @@
-import { roomsModel } from 'src/models';
-import { QueryResolvers } from '../../../generated';
- 
-export const hotelDetail: QueryResolvers['hotelDetail'] = async (_,variables) => {
-  const {hotelId} = variables;
- 
-  const rooms = await roomsModel.find({hotelId});
- 
-  if(!rooms){
-    throw new Error('Rooms not found');
+import { bookingModel, roomsModel } from 'src/models';
+import { QueryResolvers, RoomsFilterInput } from '../../../generated';
+import { FilterType } from './filter-types';
+
+export const hotelDetail: QueryResolvers['hotelDetail'] = async (_, variables) => {
+  const { hotelId, input } = variables;
+  const filterRoom = {};
+  if (input) {
+    await filterDate({ filterRoom, input });
+    filterRoomType({ filterRoom, input });
   }
+  const rooms = await roomsModel.find({ hotelId, ...filterRoom });
+
   return rooms;
+};
+
+const filterDate = async ({ filterRoom, input }: { filterRoom: FilterType; input: RoomsFilterInput }) => {
+  const { checkInDate, checkOutDate } = input;
+  let booked = [];
+  if (checkInDate && checkOutDate) {
+    booked = await bookingModel.find({
+      checkInDate: { $lt: checkOutDate },
+      checkOutDate: { $gt: checkInDate },
+    });
+  }
+
+  if (!booked.length) return;
+
+  const bookedRoomIds = booked.map((booking) => booking.roomId);
+  console.log(bookedRoomIds);
+  filterRoom._id = {
+    $nin: bookedRoomIds,
+  };
+};
+
+const filterRoomType = ({ filterRoom, input }: { filterRoom: FilterType; input: RoomsFilterInput }) => {
+  const { roomType } = input;
+  if (!roomType) return;
+  filterRoom.roomType = roomType;
 };
