d44f19e4fa8957c58ccff2c6fcf59100f34cd308
Erdenesuren Batbaatar <erdenesuren.bat2024@gmail.com>
2025-01-11 19:21:29 +0800

    remove follow request from notification by private following user id (#1446)

--- apps/1CD/instagram/backend/src/resolvers/mutations/follow/index.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/follow/index.ts
@@ -2,3 +2,4 @@ export * from './send-follow-req';
 export * from './confirm-follow-req';
 export * from './remove-follower';
 export * from './unfollow';
+export * from './remove-follow-req-from-notify-by-private-following-id-user';
