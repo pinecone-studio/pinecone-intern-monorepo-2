33cc72b73bc07cd3c99b9082e4fea69ba42f8edf
Myeruyert <myeruyertjanibyek@gmail.com>
2025-01-13 10:53:14 +0800

    fix(ig-fe): test deployment (#1514)

--- apps/1CD/instagram/backend/src/resolvers/mutations/auth/forget-password.ts
+++ apps/1CD/instagram/backend/src/resolvers/mutations/auth/forget-password.ts
@@ -6,14 +6,17 @@ import { MutationResolvers, Response } from 'src/generated';
 export const forgetPassword: MutationResolvers['forgetPassword'] = async (_: unknown, { input }) => {
   const { email } = input;
   const userExist = await userModel.findOne({ email });
-  if (!userExist) {
-    throw new Error('Can not find this email address');
-  }
+
+  if (!userExist) throw new Error('Can not find this email address');
+
   const resetToken = crypto.randomBytes(25).toString('hex');
   const hashedResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
   userExist.resetPasswordToken = hashedResetToken;
   userExist.resetPasswordTokenExpire = new Date(Date.now() + 3 * 60 * 1000);
+
   await userExist.save();
   await sendResetPassUrlToMail(email, resetToken);
+
   return Response.Success;
 };
