97630f9a1a7d9acbbe1929b39a9760b7afe899b0
badral <badral.b@nest.edu.mn>
2025-02-25 22:03:22 +0800

    1540 admin front (#1546)
    * feat: dsa
    
    f
    
    * feat: gsd
    
    sag
    
    * feat: fds
    
    * feat(fsd): fsd
    
    ---------
    
    Co-authored-by: satsuralala <satsuraltumurbat@gmail.com>

--- apps/1CD/hotel-booking/frontend/src/app/(admin)/guests/_components/DataTable.tsx
+++ apps/1CD/hotel-booking/frontend/src/app/(admin)/guests/_components/DataTable.tsx
@@ -1,13 +1,13 @@
 import { TableRow, TableCell } from '@/components/ui/table';
-import { Badge } from '@/components/ui/badge';
 import { BookingsType } from '@/generated';
+import BookingStatusBadge from '@/components/BookingStatusBadge';
 
 const DataTable = ({ bookingsData }: { bookingsData: BookingsType }) => {
   return (
     <div key={bookingsData._id} data-cy="Bookings-Data-Table-Component" className='h-full'>
       <TableRow className="flex gap-4 border">
         <TableCell className="border-r-[1px] py-3 px-4 w-[50px]">{bookingsData._id?.slice(0, 3)}</TableCell>
-        <TableCell className="border-r-[1px] flex-1">{bookingsData.userId?.firstName && bookingsData.userId.lastName && `${bookingsData.userId.firstName} ${bookingsData.userId.lastName}`}</TableCell>
+        <TableCell className="border-r-[1px] flex-1">{showName({ firstName: bookingsData?.firstName, lastName: bookingsData?.lastName, email: bookingsData?.email })}</TableCell>
         <TableCell className="border-r-[1px] flex-1 ">{bookingsData.roomId?.hotelId?.hotelName}</TableCell>
         <TableCell className="border-r-[1px] flex-1">{bookingsData.roomId?.roomName}</TableCell>
         <TableCell className="border-r-[1px] flex-1">{bookingsData.roomId?.roomType}</TableCell>
@@ -21,10 +21,16 @@ const DataTable = ({ bookingsData }: { bookingsData: BookingsType }) => {
         </TableCell>
 
         <TableCell className="flex-1 h-8">
-          <Badge className={`w-24 flex justify-center items-center ${bookingsData.status === 'completed' ? 'bg-green-500' : 'bg-red-400'}`}>{bookingsData.status}</Badge>
+          <BookingStatusBadge status={bookingsData.status} />
         </TableCell>
       </TableRow>
     </div>
   );
 };
 export default DataTable;
+
+const showName = ({ firstName, lastName, email }: { firstName: string | undefined | null; lastName: string | null | undefined; email: string | null | undefined }) => {
+  if (firstName) return firstName;
+  if (lastName) return lastName;
+  return email?.split('@')[0];
+};
--- apps/1CD/hotel-booking/frontend/src/components/BookingStatusBadge.tsx
+++ apps/1CD/hotel-booking/frontend/src/components/BookingStatusBadge.tsx
@@ -5,7 +5,7 @@ const BookingStatusBadge = ({ status }: { status: Maybe<BookingStatus> | undefin
   return (
     <Badge
       data-cy="Booking-Status-Badge"
-      className={`w-20 ${status == 'cancelled' && 'bg-[#F97316] hover:bg-[#F97316]'} ${status == 'completed' && 'bg-green-600 hover:bg-green-600'} ${
+      className={`w-20 grid place-items-center ${status == 'cancelled' && 'bg-[#F97316] hover:bg-[#F97316]'} ${status == 'completed' && 'bg-green-600 hover:bg-green-600'} ${
         status == 'booked' && 'bg-[#2563EB] hover:bg-[#2563EB]'
       }`}
     >
