4d719f8e098a01f0e78c7115709e571cd4faa07c
Second <drewuncle863724lu@gmail.com>
2025-05-23 10:58:40 +0800

    fix: preview error at ci

--- apps/2CD/tinder/frontend/codegen.ts
+++ apps/2CD/tinder/frontend/codegen.ts
@@ -2,14 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: {
-  [process.env.LOCAL_BACKEND_URI ?? "https://tinder-backend-testing-livid.vercel.app/api/graphql"]: {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  },
-},
-
+  schema: process.env.LOCAL_BACKEND_URI ?? "https://tinder-backend-testing-livid.vercel.app/api/graphql",
   documents: ['apps/2CD/tinder/frontend/src/**/*.graphql'],
   generates: {
     'apps/2CD/tinder/frontend/src/generated/index.ts': {
