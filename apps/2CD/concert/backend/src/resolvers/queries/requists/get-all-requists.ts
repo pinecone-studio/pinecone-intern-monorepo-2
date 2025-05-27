947df45d69101610935e4617cc54b599331038e7
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-05-27 17:15:38 +0800

    feat: add request queries

--- apps/2CD/concert/backend/src/resolvers/queries/requists/get-all-requists.ts
+++
@@ -1,9 +0,0 @@
-import { RequestModel } from 'src/models';
-export const getAllRequists = async () => {
-  try {
-    const getAllRequists = await RequestModel.find();
-    return getAllRequists;
-  } catch (error) {
-    throw new Error('Internal server error get all requists');
-  }
-};
