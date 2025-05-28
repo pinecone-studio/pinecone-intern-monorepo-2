11b426dad0424ae992f7819a9f37ce787fe44632
Vskroi <torb06221419@gmail.com>
2025-05-28 18:08:48 +0800

    feat: add booking update and create

--- apps/2CD/hotel/backend/src/resolvers/index.ts
+++ apps/2CD/hotel/backend/src/resolvers/index.ts
@@ -1,9 +1,13 @@
 import { addHotel } from './mutations/add-hotel';
+import { CreateBooking } from './mutations/booking/create-booking';
+import { updateBooking } from './mutations/booking/update-booking';
 import { getAllHotels } from './queries/get-all-hotels';
 
 export const resolvers = {
   Mutation: {
     addHotel,
+    CreateBooking,
+    updateBooking
   },
   Query: {
     getAllHotels,
