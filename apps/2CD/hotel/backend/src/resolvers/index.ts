f111355e3a8a0e0944fc84cb2cb37773a9135974
Gantulga0 <tudewdagwagantulga@gmail.com>
2025-05-29 19:09:37 +0800

    feat(hotel review added): added hotel review schema and mutation

--- apps/2CD/hotel/backend/src/resolvers/index.ts
+++ apps/2CD/hotel/backend/src/resolvers/index.ts
@@ -1,9 +1,11 @@
 import { addHotel } from './mutations/add-hotel';
 import { getAllHotels } from './queries/get-all-hotels';
+import reviewMutations from './mutations/review-mutations';
 
 export const resolvers = {
   Mutation: {
     addHotel,
+    ...reviewMutations,
   },
   Query: {
     getAllHotels,
--- apps/2CD/hotel/backend/src/schemas/index.ts
+++ apps/2CD/hotel/backend/src/schemas/index.ts
@@ -2,5 +2,7 @@ import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
 import {typeDefs as RoomTypeDefs} from './room.schema'
 import {typeDefs as RoomServiceTypeDefs} from './room-seervice.schema'
+import { typeDefs as ReviewTypeDefs } from './review-schema';
+import { userTypeDefs } from './user-schema';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs, RoomTypeDefs,RoomServiceTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, RoomTypeDefs,RoomServiceTypeDefs, ReviewTypeDefs, userTypeDefs]);
