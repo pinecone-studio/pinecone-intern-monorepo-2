9b929a232b3e003d8b7efd8e40e9bc0009c13f36
24LP5684 24LP5684 <24LP5684@G304-CC0217.local>
2025-05-29 12:06:16 +0800

    fix(hotel): rooom and hotel connection

--- apps/2CD/hotel/backend/src/schemas/index.ts
+++ apps/2CD/hotel/backend/src/schemas/index.ts
@@ -1,6 +1,6 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
 import {typeDefs as RoomTypeDefs} from './room.schema'
-import {typeDefs as RoomServiceTypeDefs} from './room-service.schema'
+// import {typeDefs as RoomServiceTypeDefs} from './room-service.schema'
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs, RoomTypeDefs,RoomServiceTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, RoomTypeDefs]);
