6556bb31726fcaf9471b0058510c9a5ae1171ae0
24LP7027 24LP7027 <24LP7027@admins-iMac.local>
2025-05-29 18:25:27 +0800

    feat(restaurant backend): user login validations
    user login validations

--- apps/2CD/restaurant/backend/src/resolvers/mutations/index.ts
+++ apps/2CD/restaurant/backend/src/resolvers/mutations/index.ts
@@ -1,7 +1,6 @@
 export * from './sample-mutation';
-
-export * from './foodRelatedMutations/create-food'
+export * from './foodRelatedMutations/create-food';
 export * from './categoryMutations/create-category';
 export * from './userRelatedMutations/create-user';
 export * from './userRelatedMutations/update-wallet';
-
+export * from './userRelatedMutations/user-login';
