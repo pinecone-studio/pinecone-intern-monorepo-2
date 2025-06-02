89dc2b1c9baac3ebee90961e0add69e20e32c3ab
24LP4957 24LP4957 <24LP4957@G304-CC0227.local>
2025-06-02 14:08:28 +0800

    feat: test coverage reached at required level

--- apps/2CD/tinder/frontend/src/components/providers/ApolloWrapper.tsx
+++ apps/2CD/tinder/frontend/src/components/providers/ApolloWrapper.tsx
@@ -1,38 +1,41 @@
 'use client';
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloProvider,
-} from '@apollo/client';
+import { HttpLink } from '@apollo/client';
+import { ApolloNextAppProvider, ApolloClient, InMemoryCache } from '@apollo/experimental-nextjs-app-support';
+import { PropsWithChildren } from 'react';
 import { setContext } from '@apollo/client/link/context';
-import { useMemo } from 'react';
-import { useAuth } from '@clerk/nextjs';
+import { useAuth } from "@clerk/nextjs";
 
 const uri = process.env.BACKEND_URI ?? 'http://localhost:4200/api/graphql';
 
-export const ApolloWrapper = ({ children }: { children: React.ReactNode }) => {
-  const { getToken } = useAuth();
+const makeClient = () => {
+  const httpLink = new HttpLink({
+    uri,
+    fetchOptions: { cache: 'no-store' },
+  });
 
-  const client = useMemo(() => {
-    const httpLink = new HttpLink({ uri });
 
-    const authLink = setContext(async (_, { headers }) => {
-      const token = await getToken();
-      return {
-        headers: {
-          ...headers,
-          Authorization: token ? `Bearer ${token}` : '',
-        },
-      };
-    });
 
-    return new ApolloClient({
-      link: authLink.concat(httpLink),
-      cache: new InMemoryCache(),
-    });
-  }, [getToken]);
 
-  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+
+  const authLink = setContext((_, { headers }) => {
+    const { getToken } = useAuth();
+    return getToken().then(token => ({
+      headers: {
+        ...headers,
+        Authorization: token ? `Bearer ${token}` : '',
+      },
+    }));
+  });
+
+
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+  });
+};
+
+export const ApolloWrapper = ({ children }: PropsWithChildren) => {
+  return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>;
 };
