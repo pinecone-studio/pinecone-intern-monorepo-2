da4a0e1c48fb626909526a412a3061c8feb032be
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-02 17:52:13 +0800

    feat(concert frontend): add header component

--- apps/2CD/concert/frontend/src/app/(admin)/layout.tsx
+++ apps/2CD/concert/frontend/src/app/(admin)/layout.tsx
@@ -2,7 +2,7 @@ import { ApolloWrapper } from '@/components/providers';
 import { ReactNode } from 'react';
 import { Header } from './_components/Header';
 
-const AdminLayout = ({ children }: ReactNode) => {
+const AdminLayout = ({ children }: { children: ReactNode }) => {
   return (
     <ApolloWrapper>
       <div className="bg-[#F4F4F5] h-100vh">
