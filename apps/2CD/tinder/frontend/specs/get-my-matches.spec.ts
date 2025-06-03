f686a66546bff95a39809a01641c6b264330d5d7
24LP4462 24LP4462 <24LP4462@G304-CC0229.local>
2025-06-03 09:42:01 +0800

    feat(tinder-frontend): get-my-matches test bichij coverage deer shalgasn

---
+++ apps/2CD/tinder/frontend/specs/get-my-matches.spec.ts
@@ -0,0 +1,12 @@
+// __tests__/get-my-matches.test.ts
+
+import { GET_MY_MATCHES } from "@/app/message/_utils/get-my-matches";
+import '@testing-library/jest-dom';
+
+
+describe('GraphQL Query', () => {
+  it('should define GET_MY_MATCHES query', () => {
+    expect(GET_MY_MATCHES).toBeDefined();
+    expect(typeof GET_MY_MATCHES).toBe('object');
+  });
+});
