d7a23dcbbc85edfda975a5c97135743ef6951044
Juu316 <uujuu316@gmail.com>
2025-06-03 19:05:22 +0800

    feat: added webhook that creates user in db

--- apps/2CD/hotel/backend/src/resolvers/index.ts
+++ apps/2CD/hotel/backend/src/resolvers/index.ts
@@ -4,15 +4,14 @@ import { addHotel } from './mutations/hotel/add-hotel';
 import { getAllHotels } from './queries/hotel/get-all-hotels';
 import { createRoom } from './mutations/room/create-room';
 import { getHotelById } from './queries/hotel/get-hotel-by-id';
-import {getAllRooms} from './queries/room/get-all-rooms'
+import { getAllRooms } from './queries/room/get-all-rooms';
 import { getRoomForId } from './queries/room/get-room-for-id';
-import {updateRoom} from './mutations/room/update-room'
+import { updateRoom } from './mutations/room/update-room';
 import { deleteRoom } from './mutations/room/delete-room';
 import reviewMutations from './mutations/review-mutations';
 import { updateHotel } from './mutations/hotel/update-hotel';
 import { getAllBookings } from './queries/booking/get-all-booking';
 import { getBookingById } from './queries/booking/get-booking-by-id';
-import { createUser } from './mutations/create-user';
 import { updateUserRoleToAdmin } from './mutations/update-user-role-to-admin';
 import { getAllUsers } from './queries/user/get-all-user';
 import { getUserById } from './queries/user/get-user-by-id';
@@ -27,8 +26,7 @@ export const resolvers = {
     updateRoom,
     deleteRoom,
     ...reviewMutations,
-    createUser,
-    updateUserRoleToAdmin
+    updateUserRoleToAdmin,
   },
   Query: {
     getAllHotels,
@@ -38,6 +36,6 @@ export const resolvers = {
     getAllBookings,
     getBookingById,
     getAllUsers,
-    getUserById
+    getUserById,
   },
 };
