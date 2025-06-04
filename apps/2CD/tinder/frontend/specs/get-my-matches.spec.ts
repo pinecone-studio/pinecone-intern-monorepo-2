ba4c15fec71ab4bb5a63023d25bb4d971382a341
24LP4462 24LP4462 <24LP4462@G304-CC0229.local>
2025-06-04 13:12:20 +0800

    feat(tinder-frontend): new specs test

--- apps/2CD/tinder/frontend/specs/get-my-matches.spec.ts
+++ apps/2CD/tinder/frontend/specs/get-my-matches.spec.ts
@@ -1,8 +1,4 @@
-
-
-
-
-import { GET_MY_MATCHES } from '@/app/message/_utils/get-my-matches';
+import { GET_MY_MATCHES } from "@/app/message/_utils/get-my-matches";
 import '@testing-library/jest-dom';
 
 
