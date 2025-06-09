9846d00b6b0d650b5bc6a839fcf45125da8f12af
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-09 18:16:40 +0800

    feat(concert:frontend): added featured concerts feature

--- apps/2CD/concert/frontend/src/app/_features/FeaturedConcerts.tsx
+++ apps/2CD/concert/frontend/src/app/_features/FeaturedConcerts.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-secrets/no-secrets */
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@/components/ui/carousel';
 import Image from 'next/image';
 import { format } from 'date-fns';
@@ -10,13 +9,13 @@ export const FeaturedConcerts = () => {
   const { data } = useGetFeaturedConcertQuery();
 
   return (
-    <Carousel className="w-full max-w-full  p-0">
+    <Carousel data-testid="featured-concerts" className="w-full max-w-full  p-0">
       <CarouselContent>
         {data?.getFeaturedConcerts.map((concert, index) => (
           <CarouselItem key={index}>
-            <div className="p-1  h-[600px] bg-black flex justify-center items-center">
-              <Image src={'https://res.cloudinary.com/dszot6j60/image/upload/v1749452826/image_nmkypg.webp'} fill alt={'featured concert image'} />
-              <div className="absolute z-10 flex items-center flex-col w-fit">
+            <div data-testid={`featured-concert${index}`} className="p-1  h-[600px] bg-black flex justify-center items-center">
+              <Image src={concert.thumbnailUrl} fill alt={'featured concert image'} />
+              <div className="absolute z-10 flex items-center flex-col w-full gap-3">
                 <div className="flex gap-2">
                   {concert.artists.map((artist, index) => {
                     return (
@@ -28,7 +27,7 @@ export const FeaturedConcerts = () => {
                 </div>
 
                 <p className="text-white text-8xl font-bold">{concert.title}</p>
-                <div className="flex gap-2 items-center text-white">
+                <div className="flex gap-2 items-center text-white ">
                   <Calendar size={16} />
                   {concert.schedule.map((day, index) => {
                     const date = format(new Date(day.startDate), 'MM/dd');
