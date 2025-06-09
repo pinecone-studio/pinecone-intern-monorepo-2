ac7fd856624621e1bf6f16f86672018259bc88b3
24LP9506 24LP9506 <batbayarsaruuul0@gmail.com>
2025-06-09 16:54:59 +0800

    feat(jhdf): aksjdf

--- apps/2CD/concert/frontend/src/app/_features/FeaturedConcerts.tsx
+++ apps/2CD/concert/frontend/src/app/_features/FeaturedConcerts.tsx
@@ -1,19 +1,41 @@
 /* eslint-disable no-secrets/no-secrets */
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from '@/components/ui/carousel';
 import Image from 'next/image';
+import { format } from 'date-fns';
 import { useGetFeaturedConcertQuery } from '../../generated';
+import { Badge } from '@/components/ui/badge';
+import { Calendar } from 'lucide-react';
 
 export const FeaturedConcerts = () => {
   const { data } = useGetFeaturedConcertQuery();
-  console.log(data);
 
   return (
     <Carousel className="w-full max-w-full  p-0">
       <CarouselContent>
         {data?.getFeaturedConcerts.map((concert, index) => (
           <CarouselItem key={index}>
-            <div className="p-1  h-[600px]">
+            <div className="p-1  h-[600px] bg-black flex justify-center items-center">
               <Image src={'https://res.cloudinary.com/dszot6j60/image/upload/v1749452826/image_nmkypg.webp'} fill alt={'featured concert image'} />
+              <div className="absolute z-10 flex items-center flex-col w-fit">
+                <div className="flex gap-2">
+                  {concert.artists.map((artist, index) => {
+                    return (
+                      <Badge className="bg-transparent border border-[#FAFAFA] border-opacity-20 font-light" key={index}>
+                        {artist.name}
+                      </Badge>
+                    );
+                  })}
+                </div>
+
+                <p className="text-white text-8xl font-bold">{concert.title}</p>
+                <div className="flex gap-2 items-center text-white">
+                  <Calendar size={16} />
+                  {concert.schedule.map((day, index) => {
+                    const date = format(new Date(day.startDate), 'MM/dd');
+                    return <p key={index}>{date}</p>;
+                  })}
+                </div>
+              </div>
             </div>
           </CarouselItem>
         ))}
