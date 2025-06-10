b8a536f1ccf9934299f9b2b2d092834a226dd510
24LP0058 24LP0058 <24LP0058@admins-iMac.local>
2025-06-10 16:52:09 +0800

    fix: fixed type:any LINTING ERROR
    Any bichtsen bsniig zaslaa

--- apps/2CD/hotel/backend/src/app/api/webhooks/route.ts
+++ apps/2CD/hotel/backend/src/app/api/webhooks/route.ts
@@ -20,7 +20,6 @@ export async function POST(req: NextRequest) {
     }
     return new Response('Webhook received', { status: 200 });
   } catch (err) {
-    console.error('Error verifying webhook:', err);
     return new Response('Error verifying webhook', { status: 400 });
   }
 }
