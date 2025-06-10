18f88ae5ceed4c0119653b1903d7c2853d354b42
24LP0058 24LP0058 <24LP0058@admins-iMac.local>
2025-06-10 16:34:34 +0800

    feat: updated webhooks file
    Hervee user accountaa ustgaval mongodb ees ch bas ustana

--- apps/2CD/hotel/backend/src/app/api/webhooks/route.ts
+++ apps/2CD/hotel/backend/src/app/api/webhooks/route.ts
@@ -5,6 +5,25 @@ import { connectToDb } from 'src/utils/connect-to-db';
 connectToDb();
 type EmailAddress = { email_address: string };
 
+// eslint-disable-next-line complexity
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const { id, email_addresses, username, first_name, last_name } = body.data || {};
+    const type: string = body.type;
+
+    if (type === 'user.created') {
+      return await handleUserCreated(id, email_addresses, username, first_name, last_name);
+    }
+    if (type === 'user.deleted') {
+      return await handleUserDeleted(id);
+    }
+    return new Response('Webhook received', { status: 200 });
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error verifying webhook', { status: 400 });
+  }
+}
 async function handleUserCreated(id: string, email_addresses: EmailAddress[] | undefined, username: string | null, first_name: string | null, last_name: string | null) {
   const email = email_addresses && email_addresses.length > 0 ? email_addresses[0].email_address : '';
   const newUser = new User({
@@ -17,18 +36,14 @@ async function handleUserCreated(id: string, email_addresses: EmailAddress[] | u
   await newUser.save();
   return new Response('User created in Mongodb successfully', { status: 201 });
 }
-
-export async function POST(req: NextRequest) {
+async function handleUserDeleted(id: string) {
   try {
-    const body = await req.json();
-    const { id, email_addresses, username, first_name, last_name } = body.data || {};
-    const type: string = body.type;
-    if (type === 'user.created') {
-      return await handleUserCreated(id, email_addresses, username, first_name, last_name);
+    const result = await User.findOneAndDelete({ clerkId: id });
+    if (!result) {
+      return new Response('User not found', { status: 404 });
     }
-    return new Response('Webhook received', { status: 200 });
-  } catch (err) {
-    console.error('Error verifying webhook:', err);
-    return new Response('Error verifying webhook', { status: 400 });
+    return new Response('User deleted successfully', { status: 200 });
+  } catch (error) {
+    return new Response('User deleted successfully', { status: 500 });
   }
 }
