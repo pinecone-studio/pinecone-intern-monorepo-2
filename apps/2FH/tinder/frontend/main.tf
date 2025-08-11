88e82af45b8230596abd483037ad536634f375ae
Munkh-Erdene <munkherdenem94@gmail.com>
2025-08-11 14:51:47 +0800

    Tinder project pr (#2010)
    * Tinder project pr
    
    * codegen fixed
    
    * codegen
    
    * last codegen
    
    * last commit
    
    * commit
    
    ---------
    
    Co-authored-by: 24LP5618 24LP5618 <24LP5618@G304-CC0223-2.localdomain>

---
+++ apps/2FH/tinder/frontend/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    vercel = {
+      source  = "vercel/vercel"
+      version = "~> 0.3"
+    }
+  }
+}
+
+resource "vercel_project" "fh2-Tinder-Frontend-development" {
+  name             = "fh2-tinder-frontend-development"
+  build_command    = "nx build --skip-nx-cache 2FH-Tinder-Frontend"
+  output_directory = "./dist/apps/2FH/Tinder/Frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_lBZB5j8sdaFCwB99JPeeSTzr"
+}
+resource "vercel_project" "fh2-Tinder-Frontend-prod" {
+  name             = "fh2-tinder-frontend-prod"
+  build_command    = "nx build --skip-nx-cache 2FH-Tinder-Frontend"
+  output_directory = "./dist/apps/2FH/Tinder/Frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_lBZB5j8sdaFCwB99JPeeSTzr"
+}
+resource "vercel_project" "fh2-Tinder-Frontend-testing" {
+  name             = "fh2-tinder-frontend-testing"
+  build_command    = "nx build --skip-nx-cache 2FH-Tinder-Frontend"
+  output_directory = "./dist/apps/2FH/Tinder/Frontend/.next"
+  framework        = "nextjs"
+  team_id          = "team_lBZB5j8sdaFCwB99JPeeSTzr"
+}
+
+variable "VERCEL_TOKEN" {
+  type        = string
+  description = "Optionally say something about this variable"
+}
+
+provider "vercel" {
+  # Or omit this for the api_token to be read
+  # from the VERCEL_API_TOKEN environment variable
+  api_token = var.VERCEL_TOKEN
+
+  # Optional default team for all resources
+  team = "team_lBZB5j8sdaFCwB99JPeeSTzr"
+}
