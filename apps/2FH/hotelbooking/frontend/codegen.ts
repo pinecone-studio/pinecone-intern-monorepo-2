e3304d318c8480a20623bbbbbcfb88b2f08497bf
25LP5095 25LP5095 <25LP5095@G304-CC0232.localdomain>
2025-08-12 12:01:01 +0800

    fixed vercel deployments

--- apps/2FH/hotelbooking/frontend/codegen.ts
+++ apps/2FH/hotelbooking/frontend/codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: 'apps/2FH/hotelbooking/backend/src/schemas/**/*.ts',
+  schema: process.env.BACKEND_URI ?? process.env.LOCAL_BACKEND_URL,
   documents: ['apps/2FH/hotelbooking/frontend/src/**/*.graphql'],
   generates: {
     'apps/2FH/hotelbooking/frontend/src/generated/index.ts': {
