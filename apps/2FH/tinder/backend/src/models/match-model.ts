8a4262e881f66d2e8b62edd8d5883e474c878334
Munkh-Erdene <munkherdenem94@gmail.com>
2025-08-13 17:23:38 +0800

    model update (#2058)
    Co-authored-by: 24LP5618 24LP5618 <24LP5618@G304-CC0223-2.localdomain>

---
+++ apps/2FH/tinder/backend/src/models/match-model.ts
@@ -0,0 +1,22 @@
+import { Schema, model, models, Model, Types } from "mongoose";
+ 
+export type MatchType = {
+  user1: Types.ObjectId;
+  user2: Types.ObjectId;
+  matchedAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+};
+ 
+const matchSchema = new Schema<MatchType>(
+  {
+    user1: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    user2: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    matchedAt: { type: Date, default: Date.now },
+  },
+  { timestamps: true }
+);
+ 
+export const Match: Model<MatchType> =
+  models.Match || model<MatchType>("Match", matchSchema);
+ 
