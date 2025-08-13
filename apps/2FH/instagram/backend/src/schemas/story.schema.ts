5c7b5ab1d439ce06fa38548211c12392afa08958
khulan <khlnua21@gmail.com>
2025-08-13 12:49:32 +0800

    story schema added

---
+++ apps/2FH/instagram/backend/src/schemas/story.schema.ts
@@ -0,0 +1,29 @@
+import gql from "graphql-tag";
+
+export const StoryTypeDefs = gql`
+scalar Date
+
+type Story {
+    _id: ID!
+    author: User!
+    image: String!
+    viewers: [User!]!
+    createdAt: Date!
+    expiredAt: Date!
+}
+
+input CreateStoryInput {
+    image: String
+}
+
+type Query {
+    getStoryByUserId(author: ID!): [Story!]!
+    getStoryById(_id: ID!): Story
+}
+
+type Mutation {
+    createStory(input: CreateStoryInput!): Story!
+    deleteStory(_id: ID!):  Boolean!
+    viewStory(_id: ID!): Story!
+}
+`
