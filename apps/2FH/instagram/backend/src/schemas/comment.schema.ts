bca2cfd7207ebee2a01a75085fd8e2124d7fdd9b
Nami11 <nmnaa0421@gmail.com>
2025-08-14 18:38:14 +0800

    comment-scheme.ts all mutations (#2066)
    Co-authored-by: namuunaa <nmnaa04221@gmail.com>

---
+++ apps/2FH/instagram/backend/src/schemas/comment.schema.ts
@@ -0,0 +1,30 @@
+import gql from "graphql-tag";
+
+ export const CommentTypeDefs = gql`
+  scalar Date
+
+ type Comment {
+ _id:ID!
+ author:ID!
+ postId:ID!
+ replyId:[ID!]!
+ content:String!
+ likes:[ID!]!
+ }
+
+ input CommentInput{
+ content:String!
+ }
+
+ type Mutation{
+createComment(input:CommentInput!):Comment!
+deleteComment(_id:ID!):Boolean!
+updateComment(_id:ID!, input:CommentInput!):Comment!
+ }
+
+ type Query{
+ 
+ }
+ `
+
+ 
