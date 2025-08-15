1177d2f76a9a60e5509bddd4372f7be6dac87118
puntsag <puntsagg0@gmail.com>
2025-08-15 14:31:03 +0800

    room query mutation test

--- apps/2FH/hotelbooking/backend/src/schemas/index.ts
+++ apps/2FH/hotelbooking/backend/src/schemas/index.ts
@@ -1,4 +1,7 @@
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { typeDefs as CommonTypeDefs } from './common.schema';
+import { roomSchemaTypeDefs } from './room.schema';
+import { print } from 'graphql';
 
-export const typeDefs = mergeTypeDefs([CommonTypeDefs]);
+export const typeDefs = mergeTypeDefs([CommonTypeDefs, roomSchemaTypeDefs]);
+console.log(print(typeDefs));
