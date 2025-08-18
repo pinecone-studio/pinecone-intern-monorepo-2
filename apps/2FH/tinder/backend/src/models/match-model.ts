1c77a29bf910adc18e4d0ed8bc70998112f912b1
Ulziinaran Zorig <ulziinaranzorig@gmail.com>
2025-08-18 19:35:16 +0800

    swipe resolvers tests (#2117)
    Co-authored-by: 24LP2564 24LP2564 <24LP2564@G304-CC0236.localdomain>

--- apps/2FH/tinder/backend/src/models/match-model.ts
+++ apps/2FH/tinder/backend/src/models/match-model.ts
@@ -1,8 +1,8 @@
 import { Schema, model, models, Model, Types } from "mongoose";
  
 export type MatchType = {
-  likeduser: Types.ObjectId;
-  matcheduser: Types.ObjectId;
+  likeduserId: Types.ObjectId;
+  matcheduserId: Types.ObjectId;
   matchedAt: Date;
   createdAt: Date;
   updatedAt: Date;
@@ -10,8 +10,8 @@ export type MatchType = {
  
 const matchSchema = new Schema<MatchType>(
   {
-    likeduser: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
-    matcheduser: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
+    likeduserId: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
+    matcheduserId: { type: Schema.Types.ObjectId, ref: "Profile", required: true },
     matchedAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
