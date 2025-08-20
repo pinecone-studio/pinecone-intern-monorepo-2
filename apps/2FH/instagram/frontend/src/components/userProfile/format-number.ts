87adc746ec5add26eba0c1976cfedd8423ba876f
buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
2025-08-20 16:50:39 +0800

    instagram frontend userProfile

---
+++ apps/2FH/instagram/frontend/src/components/userProfile/format-number.ts
@@ -0,0 +1,6 @@
+// src/utils/formatNumber.ts
+export const formatNumber = (num: number): string => {
+    if (num >= 1_000_000) return (num / 1_000_000).toFixed(1) + "M";
+    if (num >= 1_000) return (num / 1_000).toFixed(1) + "K";
+    return num.toString();
+  };
