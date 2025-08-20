e89cc9a9cc40063b9cd2a25cd4c2bf5960f7f93a
Buynaa192 <buynnnemehts@gmail.com>
2025-08-20 20:15:49 +0800

    instagram frontend userProfile (#2152)
    * instagram frontend userProfile
    
    * format number edit
    
    * preview e2e aldaa
    
    * all-pages.json change
    
    * bug
    
    ---------
    
    Co-authored-by: buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>

---
+++ apps/2FH/instagram/frontend/cypress/support/commands.ts
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+// Custom commands энд нэмж болно
+// Жишээ:
+/*
+Cypress.Commands.add('login', (email: string, password: string) => {
+  cy.visit('/login');
+  cy.get('[data-testid=email]').type(email);
+  cy.get('[data-testid=password]').type(password);
+  cy.get('[data-testid=submit]').click();
+});
+*/
+
+// Type definitions for custom commands
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Cypress {
+      // eslint-disable-next-line @typescript-eslint/no-empty-interface
+      interface Chainable {
+        // login(email: string, password: string): Chainable<void>;
+      }
+    }
+  }
+
+  export {};
