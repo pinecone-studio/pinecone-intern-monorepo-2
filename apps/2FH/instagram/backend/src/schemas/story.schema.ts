d1299b344738ca64bc727e7369128590fd42622b
B.Bayargun <135527106+s1nner13@users.noreply.github.com>
2025-08-26 13:27:25 +0800

    get-story author fixed (#2196)
    Co-authored-by: 24LP2084 24LP2084 <24LP2084@G304-CC0235.local>

--- apps/2FH/instagram/backend/src/schemas/story.schema.ts
+++ apps/2FH/instagram/backend/src/schemas/story.schema.ts
@@ -1,33 +1,33 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const StoryTypeDefs = gql`
-    scalar Date
+  scalar Date
 
-    type Story {
-        _id: ID!
-        author: String
-        image: String!
-        viewers: [User!]!
-        createdAt: Date!
-        expiredAt: Date!
-    }
+  type Story {
+    _id: ID!
+    author: User
+    image: String!
+    viewers: [User!]!
+    createdAt: Date!
+    expiredAt: Date!
+  }
 
-    input CreateStoryInput {
-        image: String!
-    }
+  input CreateStoryInput {
+    image: String!
+  }
 
-    type Query {
-        getStoryByUserId(author: ID!): [Story!]!
-        getStoryById(_id: ID!): Story
-        # Get current user's active stories
-        getMyStories: [Story!]!
-        # Get all active stories from followed users
-        getActiveStories: [Story!]!
-    }
+  type Query {
+    getStoryByUserId(author: ID!): [Story!]!
+    getStoryById(_id: ID!): Story
+    # Get current user's active stories
+    getMyStories: [Story!]!
+    # Get all active stories from followed users
+    getActiveStories: [Story!]!
+  }
 
-    type Mutation {
-        createStory(input: CreateStoryInput!): Story!
-        deleteStory(_id: ID!): Boolean!
-        viewStory(_id: ID!): Story!
-    }
+  type Mutation {
+    createStory(input: CreateStoryInput!): Story!
+    deleteStory(_id: ID!): Boolean!
+    viewStory(_id: ID!): Story!
+  }
 `;
