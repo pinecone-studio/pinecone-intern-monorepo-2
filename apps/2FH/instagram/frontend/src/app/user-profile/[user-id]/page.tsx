b5ff3fbdb6b4bb24bee1c8a65eb99c73fd4a410f
khulan <khlnua21@gmail.com>
2025-08-27 14:28:21 +0800

    home page (#2131)
    * home page
    
    * fixed lint error
    
    * fixed e2e
    
    * search bar added
    
    * home page
    
    * conflict zassan
    
    * conflict
    
    ---------
    
    Co-authored-by: 25LP9675 25LP9675 <25LP9675@G304-CC0220.localdomain>
    Co-authored-by: buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>

---
+++ apps/2FH/instagram/frontend/src/app/user-profile/[user-id]/page.tsx
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+    return (
+        <div>
+     <span className="text-2xl font-bold justify-center items-center">kjhgfgh</span>
+        </div>
+    )
+}
+
+export default UserProfile;
