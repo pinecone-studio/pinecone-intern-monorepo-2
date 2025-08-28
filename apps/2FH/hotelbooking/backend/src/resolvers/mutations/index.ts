86ba71ee10f240a6954969e13cff30b20af97b29
roru <roru@rorus-Laptop.local>
2025-08-28 13:14:41 +0800

    fixed index.ts

--- apps/2FH/hotelbooking/backend/src/resolvers/mutations/index.ts
+++ apps/2FH/hotelbooking/backend/src/resolvers/mutations/index.ts
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 export * from './hotel';
 export * from './emergency-contact'
 export * from './user';
 export * from './room';
 export * from './booking';
-=======
-export * from './booking';
->>>>>>> c6242500 (fixed build)
