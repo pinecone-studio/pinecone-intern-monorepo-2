d52bdd0ed726abc7bc0a181ac5725b0c289a3253
Buynaa192 <buynnnemehts@gmail.com>
2025-08-30 20:57:30 +0800

    uur hunii profile harj boldog bolson (#2247)
    * uur hunii profile harj boldog bolson
    
    * uur hunii profile harj boldog bolson
    
    * e2e aldaa zassan
    
    ---------
    
    Co-authored-by: buynaa <tserendagvabuyanzayaa@Buynaa-MacBook-Pro.local>
    Co-authored-by: aznemesis777 <obilguudei48@gmail.com>

--- apps/2FH/instagram/frontend/cypress/e2e/(main)/user-profile.cy.tsx
+++ apps/2FH/instagram/frontend/cypress/e2e/(main)/user-profile.cy.tsx
@@ -2,11 +2,13 @@
 
 describe('User Profile Page', () => {
   beforeEach(() => {
-    // Тест эхлэх бүрт user profile руу орох
+    // TODO: Fix when backend is properly configured
+    // Currently causes 500 error
     cy.visit('/userProfile');
   });
 
   it('should display username and profile info', () => {
+    // TODO: Fix when backend is properly configured
     cy.contains('travel.explorer').should('be.visible');
     cy.contains('Alex Chen').should('be.visible');
     cy.contains('Content Creator').should('be.visible');
@@ -14,25 +16,30 @@ describe('User Profile Page', () => {
   });
 
   it('should show profile picture', () => {
+    // TODO: Fix when backend is properly configured
     cy.get("img[alt='travel.explorer profile picture']").should('be.visible');
   });
 
   it('should display stats (posts, followers, following)', () => {
+    // TODO: Fix when backend is properly configured
     cy.contains('284 posts').should('be.visible');
     cy.contains('15.6K followers').should('be.visible');
     cy.contains('432 following').should('be.visible');
   });
 
   it('should have Edit Profile and Ad tools buttons', () => {
+    // TODO: Fix when backend is properly configured
     cy.contains('Edit Profile').should('be.visible');
     cy.contains('Ad tools').should('be.visible');
   });
 
   it('should have website link clickable', () => {
+    // TODO: Fix when backend is properly configured
     cy.get('a').contains('alexchen-photography.com').should('have.attr', 'href').and('include', 'https://alexchen-photography.com');
   });
 
   it('should switch between POSTS and SAVED tabs', () => {
+    // TODO: Fix when backend is properly configured
     cy.contains('POSTS').click().should('have.class', 'font-semibold');
     cy.contains('SAVED').click().should('have.text', 'SAVED');
   });
