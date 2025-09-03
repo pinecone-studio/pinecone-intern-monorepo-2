f2fb9346c1ea72a82ecc296a7920d1abb5558ae2
Unursaikhan <zyxeramv@gmail.com>
2025-09-03 15:05:25 +0800

    getme

--- apps/2FH/hotelbooking/frontend/src/app/(private)/page.tsx
+++ apps/2FH/hotelbooking/frontend/src/app/(private)/page.tsx
@@ -1,12 +1,6 @@
 'use client';
 
-import { useOtpContext } from '@/components';
-
 const Page = () => {
-  const { me } = useOtpContext();
-
-  console.log('Current user:', me);
-
   return <div className="flex justify-between w-[1280px]">Home page</div>;
 };
 
