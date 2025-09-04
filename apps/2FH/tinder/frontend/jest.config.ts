2a95eaa9131e4d221bb80a84e0e6dd813be6de6d
Bulgan Tamir <bulgantamir@Bulgans-MacBook-Air.local>
2025-09-04 17:38:40 +0800

    P

--- apps/2FH/tinder/frontend/jest.config.ts
+++ apps/2FH/tinder/frontend/jest.config.ts
@@ -2,14 +2,12 @@
 export default {
   displayName: '2FH-tinder-frontend',
   preset: '../../../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'], babelrc: false }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/apps/2FH/tinder/frontend',
-
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
     '!src/**/generated/**/*.ts',
@@ -17,7 +15,7 @@ export default {
     '!src/components/providers/*.tsx',
     '!src/utils/*.ts',
     '!src/hooks/*.ts',
-    '!src/**/*.test.{ts,tsx}',
     '!src/**/*.spec.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
   ],
 };
